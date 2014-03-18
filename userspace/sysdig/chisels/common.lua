16f7ff4e9970c33cbe5f4884385738fd77f59d0c
Loris Degioanni <loris@draios.com>
2014-03-17 22:48:52 -0700

    two more chisels: fdbytes_by and fdtime_by

--- userspace/sysdig/chisels/common.lua
+++ userspace/sysdig/chisels/common.lua
@@ -32,6 +32,14 @@ function format_bytes(val)
 end
 
 --[[ 
+convert a nanosecond time interval into a s.ns representation.
+E.g. 1100000000 becomes 1.1s
+]]--
+function format_time_interval(val)
+	return string.format("%u.%03us", val / 1000000000, (val % 1000000000) / 1000000)
+end
+
+--[[ 
 extract the top num entries from the table t, after sorting them based on the entry value using the function order()
 ]]--
 function pairs_top_by_val(t, num, order)
--- userspace/sysdig/chisels/fdtime_by.lua
+++ userspace/sysdig/chisels/fdtime_by.lua
@@ -1,6 +1,6 @@
 -- Chisel description
-description = "Gropus all the active FDs based on the given filter field, and returns the fd count for each key. For example, it can be used to list the number of connections per process or per IP endpoint.";
-short_description = "FDs group by";
+description = "Gropus FD activity based on the given filter field, and returns the keys where most time was spent. For example, this script can be used to list the processes or files that caused the biggest I/O latency.";
+short_description = "FD time group by";
 category = "IO";
 
 -- Chisel argument list
@@ -37,6 +37,10 @@ function on_init()
 	fkey = chisel.request_field(key_fld)
 	ffdnum = chisel.request_field("fd.num")
 	ffdname = chisel.request_field("fd.name")
+	flatency = chisel.request_field("evt.latency")
+	
+	-- set the filter
+	chisel.set_filter("evt.is_io=true and fd.type=file")
 	
 	return true
 end
@@ -46,22 +50,16 @@ function on_event()
 	key = evt.field(fkey)
 	fdnum = evt.field(ffdnum)
 	fdname = evt.field(ffdname)
+	latency = evt.field(flatency)
 
-	if key ~= nil and fdnum ~= nil and fdnum > 0 and fdname ~= nil and fdname ~= "" then
+	if key ~= nil and fdnum ~= nil and latency ~= nil and latency > 0 and fdnum > 0 and fdname ~= nil and fdname ~= "" then
 		entryval = grtable[key]
 		fdkey = tostring(fdnum) .. fdname
 
 		if entryval == nil then
-			grtable[key] = {}
-			grtable[key][fdkey] = 1
-			grtable[key]["c"] = 1
+			grtable[key] = latency
 		else
-			fdentry = grtable[key][fdkey]
-			
-			if fdentry == nil then
-				grtable[key][fdkey] = 1
-				grtable[key]["c"] = grtable[key]["c"] + 1
-			end
+			grtable[key] = grtable[key] + latency
 		end
 	end
 
@@ -70,12 +68,12 @@ end
 
 -- Interval callback, emits the ourput
 function on_capture_end()
-	sorted_grtable = pairs_top_by_val(grtable, TOP_NUMBER, function(t,a,b) return t[b]["c"] < t[a]["c"] end)
+	sorted_grtable = pairs_top_by_val(grtable, TOP_NUMBER, function(t,a,b) return t[b] < t[a] end)
 	
 	etime = evt.field(ftime)
 
 	for k,v in sorted_grtable do
-		print(k, v["c"])
+		print(k, format_time_interval(v))
 	end
 	
 	return true
