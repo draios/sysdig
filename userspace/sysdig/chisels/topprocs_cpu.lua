0941fc377144ee10c72c06b12d56df61e7a4d9ce
Loris Degioanni <loris@draios.com>
2014-03-25 13:29:34 -0700

    more accurate numbers in the topprocs_cpu chisel

--- userspace/sysdig/chisels/topprocs_cpu.lua
+++ userspace/sysdig/chisels/topprocs_cpu.lua
@@ -24,23 +24,94 @@ hidden = true
 -- Chisel argument list
 args = {}
 
--- The number of items to show
-TOP_NUMBER = 10
+require "common"
+terminal = require "ansiterminal"
 
--- Argument notification callback
-function on_set_arg(name, val)
-	return false
-end
+top_number = 10
+grtable = {}
+key_fld = "proc.name"
+key_desc = "Process"
+value_fld = "thread.exectime"
+value_desc = "CPU%"
+result_rendering = "timepct"
+islive = false
+lastts_s = 0
+lastts_ns = 0
+reminder = 0
 
--- Initialization callback
 function on_init()
-	chisel.exec("table_generator", 
-		"proc.name",
-		"Process",
-		"thread.exectime",
-		"CPU%",
-		"evt.type=switch", 
-		"" .. TOP_NUMBER,
-		"timepct")
+	-- Request the fields we need
+	fkey = chisel.request_field(key_fld)
+	fvalue = chisel.request_field(value_fld)
+	
+	chisel.set_filter("evt.type=switch")
+	
 	return true
 end
+
+function on_capture_start()
+	islive = sysdig.is_live()
+
+	if islive then
+		chisel.set_interval_s(1)
+		terminal.clearscreen()
+		terminal.hidecursor()
+	end
+
+	return true
+end
+
+function on_event()
+	key = evt.field(fkey)
+	value = evt.field(fvalue)
+
+	if key ~= nil and value ~= nil and value > 0 then
+		if grtable[key] == nil then
+			grtable[key] = value - reminder
+		else
+			grtable[key] = grtable[key] + value - reminder
+		end
+		
+		reminder = 0
+		
+		lastts_s, lastts_ns = evt.get_ts()
+	end
+
+	return true
+end
+
+function on_interval(ts_s, ts_ns, delta)
+	terminal.clearscreen()
+	terminal.goto(0, 0)
+	
+	if lastts_s ~= 0 then
+		reminder = 1000000000 - lastts_ns
+
+		if grtable[key] == nil then
+			grtable[key] = value
+		else
+			grtable[key] = grtable[key] + reminder
+		end
+	end
+	
+	print_sorted_table(grtable, 1000000, result_rendering)
+	
+	-- Clear the table
+	grtable = {}
+	
+	return true
+end
+
+function on_capture_end(ts_s, ts_ns, delta)
+	if islive then
+		terminal.clearscreen()
+		terminal.goto(0 ,0)
+		terminal.showcursor()
+		return true
+	end
+	
+	print_sorted_table(grtable, delta, result_rendering)
+	
+	return true
+end
+
