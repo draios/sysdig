8be7e4b6024a6213acee91cd6659731a07397378
Gianluca Borello <gianluca@draios.com>
2014-03-28 15:26:01 -0700

    typo

--- driver/ppm_events.h
+++ driver/ppm_events.h
@@ -91,7 +91,7 @@ extern const struct ppm_event_entry g_ppm_events[];
 
 /*
  * Functions
- *
+ */
 int32_t f_sys_autofill(struct event_filler_arguments *args, const struct ppm_event_entry *evinfo);
 int32_t val_to_ring(struct event_filler_arguments *args, uint64_t val, uint16_t val_len, bool fromuser);
 inline int32_t add_sentinel(struct event_filler_arguments *args);
