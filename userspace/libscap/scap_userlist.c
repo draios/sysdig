bac361ad850e35379af93c461e03d2ba7d25ea2b
Loris Degioanni <loris@draios.com>
2014-04-01 08:19:33 -0700

    GPLv2 license

--- userspace/libscap/scap_userlist.c
+++ userspace/libscap/scap_userlist.c
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2013-2014 Draios inc.
+
+This file is part of sysdig.
+
+sysdig is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+sysdig is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include "scap.h"
 #include "scap-int.h"
--- userspace/libsinsp/filter.cpp
+++ userspace/libsinsp/filter.cpp
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2013-2014 Draios inc.
+
+This file is part of sysdig.
+
+sysdig is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+sysdig is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 //
 // Why isn't this parser written using antlr or some other parser generator?
 // Essentially, after dealing with that stuff multiple times in the past, and fighting for a day
