a6da7b2d57e9f5494129ec68e1e9d23e6e3e30a7
Gianluca Borello <gianluca@draios.com>
2014-04-05 18:36:50 -0700

    Import/typedef in userspace the kernel types for all platforms

--- userspace/common/sysdig_types.h
+++ userspace/common/sysdig_types.h
@@ -30,3 +30,19 @@ along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
 #include <inttypes.h>
 #include <stdbool.h>
 #endif /* _WIN32 */
+
+//
+// Import/typedef in userspace the kernel types
+//
+#if !defined(_WIN32) && !defined(__APPLE__)
+#include <linux/types.h>
+#else 
+typedef uint64_t __u64;
+typedef int64_t __s64;
+typedef uint32_t __u32;
+typedef int32_t __s32;
+typedef uint16_t __u16;
+typedef int16_t __s16;
+typedef uint8_t __u8;
+typedef int8_t __s8;
+#endif
