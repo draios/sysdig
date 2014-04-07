b8cc599119d9d5765941c7ea3164bb26a209902c
Riccardo Schirone <sirmy15@gmail.com>
2014-04-07 10:49:56 +0200

    userspace/libsinsp/parsers.cpp: filter out events about sysdig digs sysdig

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -68,6 +68,16 @@ void sinsp_parser::process_event(sinsp_evt *evt)
 	reset(evt);
 
 	//
+	// Filter out event about sysdig itself
+	//
+	if(m_inspector->is_live() && evt->get_thread_info() != NULL &&
+		evt->get_thread_info()->m_pid == getpid())
+	{
+		evt->m_filtered_out = true;
+		return;
+	}
+
+	//
 	// Filtering
 	//
 #if defined(HAS_FILTERING) && defined(HAS_CAPTURE_FILTERING)
