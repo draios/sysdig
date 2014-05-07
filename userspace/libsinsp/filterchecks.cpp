d8c983bfcfebabc7389afbbb0bd62bf80ad55e11
Loris Degioanni <loris@draios.com>
2014-05-07 15:54:35 -0700

    a bit more space for names in the -l command line switch

--- userspace/libsinsp/filterchecks.cpp
+++ userspace/libsinsp/filterchecks.cpp
@@ -585,6 +585,29 @@ uint8_t* sinsp_filter_check_fd::extract(sinsp_evt *evt, OUT uint32_t* len)
 			return (uint8_t*)&m_tbool;
 		}
 		break;
+	case TYPE_SOCKFAMILY:
+		{
+			if(m_fdinfo == NULL)
+			{
+				return NULL;
+			}
+
+			if(m_fdinfo->m_type == SCAP_FD_IPV4_SOCK || m_fdinfo->m_type == SCAP_FD_IPV6_SOCK)
+			{
+				m_tstr = "ip";
+				return (uint8_t*)m_tstr.c_str();
+			}
+			else if(m_fdinfo->m_type == SCAP_FD_IPV4_SOCK || m_fdinfo->m_type == SCAP_FD_IPV6_SOCK)
+			{
+				m_tstr = "unix";
+				return (uint8_t*)m_tstr.c_str();
+			}
+			else
+			{
+				return NULL;
+			}
+		}
+		break;
 	default:
 		ASSERT(false);
 	}
