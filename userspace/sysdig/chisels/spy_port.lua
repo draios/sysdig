c78abd764486a2f7afc10e806fb9c91e4a1a1cb4
Erethon <info@erethon.com>
2014-05-09 01:29:02 +0300

    chisels: Various small fixes to chisels
    * spy_users: Add a space after the username to make output more
    readable
    * spy_ip: Update -T to -A in description to be in line with bef6091
    * spy_port: Update -T to -A in description to be in line with bef6091

--- userspace/sysdig/chisels/spy_port.lua
+++ userspace/sysdig/chisels/spy_port.lua
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 -- Chisel description
-description = "shows the network payloads exchanged using a given IP port number. You can combine this chisel with the -x, -X or -T sysdig command line switches to customize the screen output";
+description = "shows the network payloads exchanged using a given IP port number. You can combine this chisel with the -x, -X or -A sysdig command line switches to customize the screen output";
 short_description = "Show the data exchanged using the given IP port number";
 category = "Net";
 
@@ -24,7 +24,7 @@ category = "Net";
 args = 
 {
 	{
-		name = "host_ip", 
+		name = "host_port",
 		description = "the remote host IP port number", 
 		argtype = "int"
 	},
