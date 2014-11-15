46e93214615ddde92939e9eea7c3788608b3aa36
Jonathan Cua <jcua@eventbrite.com>
2014-11-15 11:40:43 -0800

    initial syscall_summary script

---
+++ userspace/sysdig/chisels/syscall_summary.lua
@@ -0,0 +1,52 @@
+-- Chisel description
+description = "Show summary of counts of system calls. You can use filters to restrict this to a specific process, thread or file."
+short_description = "Summary of counts of system calls"
+category = "Performance"
+
+-- Chisel argument list
+args = {}
+
+record_for = {}
+terminal = require "ansiterminal"
+
+-- Argument notification callback
+function on_set_arg(name, val)
+    return false
+end
+
+-- Initialization callback
+function on_init()
+    fevtype = chisel.request_field("evt.type")
+    fcount = chisel.request_field("evt.count")
+    return true
+end
+
+-- Event parsing callback
+function on_event()
+    evtype = evt.field(fevtype)
+    count = evt.field(fcount)
+
+    if evtype == "switch" then
+        return true
+    end
+
+    if record_for[evtype] == nil then
+        record_for[evtype] = count
+    else
+        record_for[evtype] = record_for[evtype] + 1
+    end
+    return true
+end
+
+-- Interval callback, emits the output
+function on_capture_end()
+    terminal.clearscreen()
+    terminal.moveto(0 ,0)
+    terminal.showcursor()
+
+    for k, v in pairs(record_for) do
+        line = string.format("%5d   %-10s", v, k)
+        print(line)
+    end
+    return true
+end
