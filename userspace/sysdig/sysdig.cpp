e39762f79c68d9ef2b5d1108af447424df46f279
Gianluca Borello <gianluca@draios.com>
2014-12-11 15:39:10 -0800

    Switch the default output format to a container-friendly one, for my tests

--- userspace/sysdig/sysdig.cpp
+++ userspace/sysdig/sysdig.cpp
@@ -729,8 +729,7 @@ sysdig_init_res sysdig_init(int argc, char **argv)
 		{0, 0, 0, 0}
 	};
 
-	output_format = "*%evt.num <TIME> %evt.cpu %proc.name (%thread.tid) %evt.dir %evt.type %evt.info";
-//	output_format = DEFAULT_OUTPUT_STR;
+	output_format = "*%evt.num <TIME> %evt.cpu %thread.container (%thread.vtid) %proc.name (%thread.tid) %evt.dir %evt.type %evt.info";
 
 	try
 	{
