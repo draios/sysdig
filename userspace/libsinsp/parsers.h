ce01f7210754e0aae7fe19c7f55f3779c4f44351
Arun Prakash Jana <engineerarun@gmail.com>
2015-02-15 10:55:23 +0530

    Removed userspace parser as no extra processing is necessary in userspace. Removed redundant printfs from userspace.
    Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

--- userspace/libsinsp/parsers.h
+++ userspace/libsinsp/parsers.h
@@ -102,7 +102,6 @@ private:
 	void parse_setresgid_exit(sinsp_evt* evt);
 	void parse_setuid_exit(sinsp_evt* evt);
 	void parse_setgid_exit(sinsp_evt* evt);
-	void parse_signal_deliver(sinsp_evt* evt);
 
 	inline void add_socket(sinsp_evt* evt, int64_t fd, uint32_t domain, uint32_t type, uint32_t protocol);
 	inline void add_pipe(sinsp_evt *evt, int64_t tid, int64_t fd, uint64_t ino);
