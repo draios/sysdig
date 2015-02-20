56587231075648c8a9bb4b3030433e3d5abdf582
Gianluca Borello <gianluca@draios.com>
2015-02-19 18:35:40 -0800

    Remove agent stuff

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 #set -e
 
-if [ ! -z "$ACCESS_KEY" ]; then
-	echo "* Setting access key"
-	
-	if ! grep ^customerid /opt/draios/bin/dragent.properties > /dev/null 2>&1; then
-		echo "customerid = $ACCESS_KEY" >> /opt/draios/bin/dragent.properties
-	else
-		sed -i "s/^customerid.*/customerid = $ACCESS_KEY/g" /opt/draios/bin/dragent.properties
-	fi
-fi
+/usr/bin/sysdig-probe-installer
 
-if [ ! -z "$TAGS" ]; then
-	echo "* Setting tags"
-
-	if ! grep ^ui.tags /opt/draios/bin/dragent.properties > /dev/null 2>&1; then
-		echo "ui.tags = $TAGS" >> /opt/draios/bin/dragent.properties
-	else
-		sed -i "s/^ui\.tags.*/ui.tags = $TAGS/g" /opt/draios/bin/dragent.properties
-	fi
-fi
-
-if [ $# -eq 0 ]; then
-	if ! /usr/bin/sysdig-probe-installer; then
-		exit 1
-	fi
-
-	exec /opt/draios/bin/dragent
-else
-	exec "$@"
-fi
+exec "$@"
