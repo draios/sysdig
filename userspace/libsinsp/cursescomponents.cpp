782c5277e6e14bd449803267913c28edd28955a3
Loris Degioanni <loris@draios.com>
2015-05-28 15:37:37 -0700

    removed the old connections view + minor help page improvements

--- userspace/libsinsp/cursescomponents.cpp
+++ userspace/libsinsp/cursescomponents.cpp
@@ -1542,7 +1542,7 @@ curses_mainhelp_page::curses_mainhelp_page(sinsp_cursesui* parent)
 	wattrset(m_win, parent->m_colors[sinsp_cursesui::PROCESS_MEGABYTES]);
 	m_ctext->printf(" Arrows");
 	wattrset(m_win, parent->m_colors[sinsp_cursesui::PROCESS]);
-	m_ctext->printf(": scroll up and down       ");
+	m_ctext->printf(": scroll the page          ");
 
 	wattrset(m_win, parent->m_colors[sinsp_cursesui::PROCESS_MEGABYTES]);
 	m_ctext->printf("CTRL+F /");
