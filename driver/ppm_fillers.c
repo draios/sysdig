d9d7985f870e871485ca5135b20abbc6f4433162
michael-trapp <michael.trapp@freenet.de>
2015-08-07 14:17:01 +0200

    - moved sembuf access to exit function - access to userspace with ppm_copy_from_user()

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -344,10 +344,10 @@ const struct ppm_event_entry g_ppm_events[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_SETNS_X] = {PPM_AUTOFILL, 1, APT_REG, {{AF_ID_RETVAL} } },
 	[PPME_SYSCALL_FLOCK_E] = {f_sys_flock_e},
 	[PPME_SYSCALL_FLOCK_X] = {PPM_AUTOFILL, 1, APT_REG, {{AF_ID_RETVAL} } },
-        [PPME_SYSCALL_SEMOP_E] = {f_sys_semop_e},
-        [PPME_SYSCALL_SEMOP_X] = {f_sys_semop_x},
-        [PPME_SYSCALL_SEMCTL_E] = {f_sys_semctl_e},
-        [PPME_SYSCALL_SEMCTL_X] = {f_sys_semctl_x},
+	[PPME_SYSCALL_SEMOP_E] = {f_sys_semop_e},
+	[PPME_SYSCALL_SEMOP_X] = {f_sys_semop_x},
+	[PPME_SYSCALL_SEMCTL_E] = {f_sys_semctl_e},
+	[PPME_SYSCALL_SEMCTL_X] = {f_sys_semctl_x},
 };
 
 /*
@@ -4528,7 +4528,6 @@ static int f_sys_semop_e(struct event_filler_arguments *args)
 {
 	unsigned long val;
 	int res;
-        struct sembuf *ptr;
 
 	/*
 	 * semid
@@ -4538,80 +4537,85 @@ static int f_sys_semop_e(struct event_filler_arguments *args)
 	if (unlikely(res != PPM_SUCCESS))
 		return res;
 
-	/*
-	 * sembuf
-	 */
-	syscall_get_arguments(current, args->regs, 1, 1, (unsigned long*) &ptr);
-
-	/*
-	 * nsops
-	 */
-	syscall_get_arguments(current, args->regs, 2, 1, &val);
-	res = val_to_ring(args, val, 0, true, 0);
-	if (unlikely(res != PPM_SUCCESS))
-		return res;
-
-	if (val && ptr)
-        {
-            // max length of sembuf array in g_event_info = 2
-            const unsigned max_nsops = 2;
-            struct sembuf dummy = {0, 0, 0};
-            unsigned j;
-
-            for(j=0; j<max_nsops; j++)
-            {
-                struct sembuf *p = &dummy;
-                if (val--)
-                    p = &ptr[j];
-
-                res = val_to_ring(args, p->sem_num, 0, true, 0);
-                if (unlikely(res != PPM_SUCCESS))
-                    return res;
-
-                res = val_to_ring(args, p->sem_op, 0, true, 0);
-                if (unlikely(res != PPM_SUCCESS))
-                    return res;
-
-                res = val_to_ring(args, semop_flags_to_scap(p->sem_flg), 0, true, 0);
-                if (unlikely(res != PPM_SUCCESS))
-                    return res;
-            }
-        }
-
 	return add_sentinel(args);
 }
 
 static int f_sys_semop_x(struct event_filler_arguments *args)
 {
+	unsigned long nsops, len;
 	int res;
 	int64_t retval;
+	struct sembuf *ptr;
 
+	/*
+	 * return value
+	 */
 	retval = (int64_t)syscall_get_return_value(current, args->regs);
 	res = val_to_ring(args, retval, 0, false, 0);
 	if (unlikely(res != PPM_SUCCESS))
 		return res;
+
+	/*
+	 * nsops
+	 * actually this could be read in the enter function but
+	 * we also need to know the value to access the sembuf structs
+	 */
+	syscall_get_arguments(current, args->regs, 2, 1, &nsops);
+	res = val_to_ring(args, nsops, 0, true, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * sembuf
+	 */
+	syscall_get_arguments(current, args->regs, 1, 1, (unsigned long*) &ptr);
+
+	if (nsops && ptr) {
+		// max length of sembuf array in g_event_info = 2
+		const unsigned max_nsops = 2;
+		unsigned       j;
+
+		for(j=0; j<max_nsops; j++) {
+			struct sembuf sops = {0, 0, 0};
+
+			if (nsops--)
+				if (unlikely(ppm_copy_from_user(&sops, (void *)&ptr[j], sizeof(struct sembuf))))
+					return PPM_FAILURE_INVALID_USER_MEMORY;
+
+			res = val_to_ring(args, sops.sem_num, 0, true, 0);
+			if (unlikely(res != PPM_SUCCESS))
+				return res;
+
+			res = val_to_ring(args, sops.sem_op, 0, true, 0);
+			if (unlikely(res != PPM_SUCCESS))
+				return res;
+
+			res = val_to_ring(args, semop_flags_to_scap(sops.sem_flg), 0, true, 0);
+			if (unlikely(res != PPM_SUCCESS))
+				return res;
+		}
+	}
 
 	return add_sentinel(args);
 }
 
 static inline u32 semctl_cmd_to_scap(unsigned cmd)
 {
-    switch (cmd)
-    {
-        case IPC_STAT: return PPM_IPC_STAT;
-        case IPC_SET: return PPM_IPC_SET;
-        case IPC_RMID: return PPM_IPC_RMID;
-        case IPC_INFO: return PPM_IPC_INFO;
-        case SEM_INFO: return PPM_SEM_INFO;
-        case SEM_STAT: return PPM_SEM_STAT;
-        case GETALL: return PPM_GETALL;
-        case GETNCNT: return PPM_GETNCNT;
-        case GETPID: return PPM_GETPID;
-        case GETVAL: return PPM_GETVAL;
-        case GETZCNT: return PPM_GETZCNT;
-        case SETALL: return PPM_SETALL;
-        case SETVAL: return PPM_SETVAL;
-    }
+	switch (cmd) {
+	case IPC_STAT: return PPM_IPC_STAT;
+	case IPC_SET: return PPM_IPC_SET;
+	case IPC_RMID: return PPM_IPC_RMID;
+	case IPC_INFO: return PPM_IPC_INFO;
+	case SEM_INFO: return PPM_SEM_INFO;
+	case SEM_STAT: return PPM_SEM_STAT;
+	case GETALL: return PPM_GETALL;
+	case GETNCNT: return PPM_GETNCNT;
+	case GETPID: return PPM_GETPID;
+	case GETVAL: return PPM_GETVAL;
+	case GETZCNT: return PPM_GETZCNT;
+	case SETALL: return PPM_SETALL;
+	case SETVAL: return PPM_SETVAL;
+	}
     return 0;
 }
 
