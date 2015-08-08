8e0e8ed2171193e98df502caf6d2fc89c214591b
Loris Degioanni <loris@draios.com>
2015-08-08 13:24:39 -0700

    marker parser bugfix

--- userspace/libsinsp/markers.cpp
+++ userspace/libsinsp/markers.cpp
@@ -637,6 +637,7 @@ inline void sinsp_markerparser::bin_parse(char* evtstr, uint32_t evtstrlen)
 
 			if(*p == ':')
 			{
+				*p = 0;
 				m_res = sinsp_markerparser::RES_OK;
 				break;
 			}
@@ -1098,7 +1099,7 @@ void sinsp_markerparser::test()
 {
 //	char doc[] = "[\">\\\"\", 12435, [\"mysql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
 //	char doc1[] = "[\"<t\", 12435, [\"mysql\", \"query\", \"init\"], []]";
-	char doc[] = ">:12345:mysql:argname1=argval1,argname2=argval2,argname3=argval3:";
+	char doc[] = ">:12345:mysql:argname1=argval1:";
 	char doc1[] = ":12345:mysql:argname1=argval1,argname2=argval2,argname3=argval3";
 
 	sinsp_threadinfo tinfo;
