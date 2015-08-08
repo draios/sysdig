fcedc097dd04ea3075f9e9ec5d33486a55b35e06
Luca Marturana <luca@sysdig.com>
2015-08-08 21:36:42 +0200

    Code deduplication

--- userspace/sysdig/chisels/httptail.lua
+++ userspace/sysdig/chisels/httptail.lua
@@ -19,112 +19,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 description = "Show a \"tail\" of HTTP transactions";
 short_description = "Tail of http transactions";
 category = "Net";
+args = {}
 
--- Chisel argument list
-args = {
-    {
-        name = "by",
-        description = "Show top HTTP transactions by: ncalls, time or bytes, default is ncalls",
-        argtype = "string",
-        optional = true
-    },
-}
-
+require "http"
 
 -- Initialization callback
 function on_init()
-
+    http_init()
     -- The -pc or -pcontainer options was supplied on the cmd line
     print_container = sysdig.is_print_container_data()
 
-    chisel.set_filter("evt.is_io = true and evt.buflen.net > 0 and fd.type = ipv4")
-    buffer_field = chisel.request_field("evt.buffer")
-    fd_field = chisel.request_field("fd.num")
-    pid_field = chisel.request_field("proc.pid")
-    rawtime_field = chisel.request_field("evt.rawtime")
-    buflen_field = chisel.request_field("evt.buflen.net")
-    datetime_field = chisel.request_field("evt.datetime")
-    dir_field = chisel.request_field("evt.io_dir")
-
-    if print_container then
-        container_field = chisel.request_field("container.name")
-    end
-
-    sysdig.set_snaplen(1024)
     return true
 end
 
 islive = false
-partial_transactions = {}
 
-function parse_request(req_buffer)
-    method, url = string.match(req_buffer, "^(%u+) (%g+)")
-    if method and url then
-        host = string.match(req_buffer, "Host: (%g+)%.%.")
-        if host then
-            url = host .. url
-        end
-        return {
-            method=method,
-            url=url
-        }
-    end
-
-    return nil
-end
-
-function parse_response(resp_buffer)
-    resp_code = string.match(resp_buffer, "HTTP/[%g]+ (%d+)")
-    if resp_code then
-        return tonumber(resp_code)
+function on_transaction(transaction)
+    if print_container then
+        container = " " .. transaction["container"] .. " "
     else
-        return nil
+        container = " "
     end
-end
-
-function on_event()
-    buf = evt.field(buffer_field)
-    fd = evt.field(fd_field)
-    pid = evt.field(pid_field)
-    evt_dir = evt.field(dir_field)
-    key = string.format("%d\001\001%d", pid, fd)
-
-    timestamp = evt.field(rawtime_field)
-    buflen = evt.field(buflen_field)
-
-    transaction = partial_transactions[key]
-    if not transaction then
-        request = parse_request(buf)
-        if request then
-            transaction_dir = "<NA>"
-            if evt_dir == "read" then
-                transaction_dir = "<"
-            elseif evt_dir == "write" then
-                transaction_dir = ">"
-            end
-            partial_transactions[key] = {
-                ts= timestamp,
-                request= request,
-                request_len=buflen,
-                dir=transaction_dir
-            }
-            if print_container then
-                partial_transactions[key]["container"] = evt.field(container_field)
-            end
-        end
-    else
-        response = parse_response(buf)
-        if response then
-            print(string.format("%s %s method=%s url=%s response_code=%d latency=%dms size=%dB",
+    print(string.format("%s%s%s method=%s url=%s response_code=%d latency=%dms size=%dB",
                 evt.field(datetime_field),
+                container,
                 transaction["dir"],
                 transaction["request"]["method"],
                 transaction["request"]["url"],
-                response,
-                (timestamp - transaction["ts"])/1000000,
-                transaction["request_len"] + buflen
+                transaction["response_code"],
+                (transaction["response_ts"] - transaction["ts"])/1000000,
+                transaction["request_len"] + transaction["response_len"]
             ))
-            partial_transactions[key] = nil
-        end
-    end
+end
+
+function on_event()
+    run_http_parser(evt, on_transaction)
 end
--- userspace/sysdig/chisels/httptop.lua
+++ userspace/sysdig/chisels/httptop.lua
@@ -32,6 +32,7 @@ args = {
 
 require "common"
 terminal = require "ansiterminal"
+require "http"
 
 vizinfo =
 {
@@ -73,56 +74,21 @@ end
 
 -- Initialization callback
 function on_init()
-
+    http_init()
     -- The -pc or -pcontainer options was supplied on the cmd line
     print_container = sysdig.is_print_container_data()
 
-    chisel.set_filter("evt.is_io = true and evt.buflen.net > 0 and fd.type = ipv4")
-    buffer_field = chisel.request_field("evt.buffer")
-    fd_field = chisel.request_field("fd.num")
-    pid_field = chisel.request_field("proc.pid")
-    rawtime_field = chisel.request_field("evt.rawtime")
-    buflen_field = chisel.request_field("evt.buflen.net")
-    dir_field = chisel.request_field("evt.io_dir")
-
     if print_container then
-        container_field = chisel.request_field("container.name")
         table.insert(vizinfo["key_fld"], 1, "container")
         table.insert(vizinfo["key_desc"], 1, "container")
     end
 
-    sysdig.set_snaplen(1024)
     return true
 end
 
 islive = false
 grtable = {}
 partial_transactions = {}
-
-function parse_request(req_buffer)
-    method, url = string.match(req_buffer, "^(%u+) (%g+)")
-    if method and url then
-        host = string.match(req_buffer, "Host: (%g+)%.%.")
-        if host then
-            url = host .. url
-        end
-        return {
-            method=method,
-            url=url
-        }
-    end
-
-    return nil
-end
-
-function parse_response(resp_buffer)
-    resp_code = string.match(resp_buffer, "HTTP/[%g]+ (%d+)")
-    if resp_code then
-        return tonumber(resp_code)
-    else
-        return nil
-    end
-end
 
 function build_grtable_key(transaction)
     request = transaction["request"]
@@ -134,56 +100,16 @@ function build_grtable_key(transaction)
     return ret
 end
 
-function on_event()
-    buf = evt.field(buffer_field)
-    fd = evt.field(fd_field)
-    pid = evt.field(pid_field)
-    evt_dir = evt.field(dir_field)
-    key = string.format("%d\001\001%d", pid, fd)
-
-    timestamp = evt.field(rawtime_field)
-    buflen = evt.field(buflen_field)
-    transaction = partial_transactions[key]
-    if not transaction then
-        request = parse_request(buf)
-        if request then
-            transaction_dir = "<NA>"
-            if evt_dir == "read" then
-                transaction_dir = "server"
-            elseif evt_dir == "write" then
-                transaction_dir = "client"
-            end
-            partial_transactions[key] = {
-                ts= timestamp,
-                request= request,
-                request_len=buflen,
-                dir=transaction_dir
-            }
-            if print_container then
-                partial_transactions[key]["container"] = evt.field(container_field)
-            end
-        end
-    else
-        response = parse_response(buf)
-        if response then
-            --print(string.format("%s %s -> %d %d ms %d bytes",
-            --    transaction["request"]["method"],
-            --    transaction["request"]["url"],
-            --    response,
-            --    (timestamp - transaction["ts"])/1000000,
-            --    transaction["request_len"] + buflen
-            --))
-            transaction["response_ts"] = timestamp
-            transaction["response_len"] = buflen
-            transaction["response_code"] = response
-            grtable_key = build_grtable_key(transaction)
-            if not grtable[grtable_key] then
-                grtable[grtable_key] = {}
-            end
-            table.insert(grtable[grtable_key], transaction)
-            partial_transactions[key] = nil
-        end
+function on_transaction(transaction)
+    grtable_key = build_grtable_key(transaction)
+    if not grtable[grtable_key] then
+        grtable[grtable_key] = {}
     end
+    table.insert(grtable[grtable_key], transaction)
+end
+
+function on_event()
+    run_http_parser(evt, on_transaction)
 end
 
 -- Final chisel initialization
