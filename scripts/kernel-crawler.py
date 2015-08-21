5cd5f88894128935ac034e8443f60ae7c2921167
Samuele <pillerisamuele@gmail.com>
2015-08-21 14:40:18 -0700

    Removed double Fedora.

--- scripts/kernel-crawler.py
+++ scripts/kernel-crawler.py
@@ -99,35 +99,6 @@ repos = {
 			"subdirs" : [
 				"Everything/x86_64/os/Packages/k/"
 			],
-			"page_pattern" : "/html/body//a[regex:test(@href, '^kernel-(devel-)?[0-9].*\.rpm$')]/@href"
-		},
-
-		{
-			"root" : "https://mirrors.kernel.org/fedora/updates/",
-			"discovery_pattern": "/html/body//a[regex:test(@href, '^2[2-9]/$')]/@href",
-			"subdirs" : [
-				"x86_64/k/"
-			],
-			"page_pattern" : "/html/body//a[regex:test(@href, '^kernel-(devel-)?[0-9].*\.rpm$')]/@href"
-		},
-
-		{
-			"root" : "https://mirrors.kernel.org/fedora/development/",
-			"discovery_pattern": "/html/body//a[regex:test(@href, '^2[2-9]/$')]/@href",
-			"subdirs" : [
-				"x86_64/os/Packages/k/"
-			],
-			"page_pattern" : "/html/body//a[regex:test(@href, '^kernel-(devel-)?[0-9].*\.rpm$')]/@href"
-		}
-	],
-
-	"Fedora" : [
-		{
-			"root" : "https://mirrors.kernel.org/fedora/releases/",
-			"discovery_pattern": "/html/body//a[regex:test(@href, '^2[2-9]/$')]/@href",
-			"subdirs" : [
-				"Everything/x86_64/os/Packages/k/"
-			],
 			"page_pattern" : "/html/body//a[regex:test(@href, '^kernel-(core|devel)-[0-9].*\.rpm$')]/@href"
 		},
 
