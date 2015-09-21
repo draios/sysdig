9a2bd4e2000ae32ee5cd9aaefe3f2e2a67ee3e25
Gianluca Borello <gianluca@draios.com>
2015-09-21 12:36:07 -0700

    More substitutions

--- userspace/libscap/scap.c
+++ userspace/libscap/scap.c
@@ -196,7 +196,7 @@ scap_t* scap_open_live_int(char *error,
 			}
 			else
 			{
-				snprintf(error, SCAP_LASTERR_SIZE, "error opening device %s. Make sure you have root credentials and that the sysdig-probe module is loaded.", filename);
+				snprintf(error, SCAP_LASTERR_SIZE, "error opening device %s. Make sure you have root credentials and that the " PROBE_NAME " module is loaded.", filename);
 			}
 
 			scap_close(handle);
