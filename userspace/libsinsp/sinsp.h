6289543118d50dac06134a82551652784c6e2cd9
Alex Fabijanic <alex@pocoproject.org>
2015-10-21 16:20:13 -0500

    add k8s watch thread disabling facilities

--- userspace/libsinsp/sinsp.h
+++ userspace/libsinsp/sinsp.h
@@ -851,4 +851,18 @@ private:
 	template<class TKey,class THash,class TCompare> friend class sinsp_connection_manager;
 };
 
+//
+// Macros for enable/disable k8s threading
+// Used to eliminate mutex locking when running single-threaded
+//
+
+#ifndef K8S_DISABLE_THREAD
+#include <mutex>
+#define K8S_DECLARE_MUTEX mutable std::mutex m_mutex
+#define K8S_LOCK_GUARD_MUTEX std::lock_guard<std::mutex> lock(m_mutex)
+#else
+#define K8S_DECLARE_MUTEX
+#define K8S_LOCK_GUARD_MUTEX
+#endif // K8S_DISABLE_THREAD
+
 /*@}*/
