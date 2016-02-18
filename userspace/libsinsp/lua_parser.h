6ff72b6f50ef9f97f5f2cb0411c0ccd68fe2530a
Henri DF <henridf@gmail.com>
2016-02-17 20:18:48 -0800

    First/partial implementation of lua parser support

---
+++ userspace/libsinsp/lua_parser.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string.h>
+
+#include "sinsp.h"
+#include "filterchecks.h"
+
+typedef struct lua_State lua_State;
+
+class lua_parser
+{
+public:
+	lua_parser(sinsp* inspector, string filename);
+	~lua_parser();
+	void load(string cmdstr);
+	sinsp_filter_expression* m_filter;
+
+ private:
+	sinsp* m_inspector;
+	bool m_lua_has_load_rules;
+	lua_State* m_ls;
+	string m_filename;
+
+	friend class lua_parser_cbacks;
+};
+
