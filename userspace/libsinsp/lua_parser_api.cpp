a5aeae39d8e9b435fa69b745d73db2f8ad0b257d
Henri DF <henridf@gmail.com>
2016-02-18 22:17:00 -0800

    Add `filter.bool_op` to Lua API
    Also rename filter.make_filter_check->filter.rel_expr

--- userspace/libsinsp/lua_parser_api.cpp
+++ userspace/libsinsp/lua_parser_api.cpp
@@ -10,6 +10,8 @@ extern "C" {
 
 extern sinsp_filter_check_list g_filterlist;
 
+// It would be nice to expose this up to Lua so that comparison operator
+// parsing/encoding can be done there.
 cmpop string_to_cmpop(const char* str)
 {
 	if(strcmp(str, "=") == 0)
@@ -58,8 +60,42 @@ cmpop string_to_cmpop(const char* str)
 	}
 }
 
+boolop string_to_boolop(const char* str)
+{
+	if(strcmp(str, "or") == 0)
+	{
+		return BO_OR;
+	}
+	else if(strcmp(str, "and") == 0)
+	{
+		return BO_AND;
+	}
+	else if(strcmp(str, "not") == 0)
+	{
+		return BO_NOT;
+	}
+	else
+	{
+		throw sinsp_exception("filter error: invalid boolean operator: " + string(str));
+	}
+}
 
-int lua_parser_cbacks::make_filter_check(lua_State *ls)
+int lua_parser_cbacks::bool_op(lua_State *ls)
+{
+	lua_getglobal(ls, "siparser");
+
+	lua_parser* parser = (lua_parser*)lua_touserdata(ls, -1);
+	lua_pop(ls, 1);
+
+	const char* opstr = luaL_checkstring(ls, 1);
+	boolop op = string_to_boolop(opstr);
+
+	parser->m_last_boolop = op;
+	return 0;
+
+}
+
+int lua_parser_cbacks::rel_expr(lua_State *ls)
 {
 	lua_getglobal(ls, "siparser");
 
@@ -73,16 +109,17 @@ int lua_parser_cbacks::make_filter_check(lua_State *ls)
 	sinsp_filter_check* chk = g_filterlist.new_filter_check_from_fldname(fld,
 									     inspector,
 									     true);
-
 	if(chk == NULL)
 	{
-		string err = "make_filter_check called with nonexistent field " + string(fld);
+		string err = "filter_check called with nonexistent field " + string(fld);
 		fprintf(stderr, "%s\n", err.c_str());
 		throw sinsp_exception("parser API error");
 	}
 
 	try
 	{
+		chk->m_boolop = parser->m_last_boolop;
+
 		chk->parse_field_name(fld, true);
 
 		const char* cmpop = luaL_checkstring(ls, 2);
@@ -101,9 +138,9 @@ int lua_parser_cbacks::make_filter_check(lua_State *ls)
 		throw e;
 	}
 
-	filter->push_expression(BO_NONE);
+	//	filter->push_expression(BO_NONE);
 	filter->add_check(chk);
-	filter->pop_expression();
+	//	filter->pop_expression();
 
 	return 0;
 }
