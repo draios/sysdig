c574b00314c770a0ff2b88362fd6d3c2778d375f
Henri DF <henridf@gmail.com>
2016-02-20 16:18:20 -0800

    lua_parser: don't create lua_state
    Instead, pass it in to constructor. That way the 'client' of this class
    just uses this as a way to add the filter API.

--- userspace/libsinsp/lua_parser.h
+++ userspace/libsinsp/lua_parser.h
@@ -10,11 +10,9 @@ typedef struct lua_State lua_State;
 class lua_parser
 {
 public:
-	lua_parser(sinsp* inspector);
+	lua_parser(sinsp* inspector, lua_State *ls);
 	~lua_parser();
 	sinsp_filter* get_filter();
-
-	lua_State* m_ls;
 
  private:
 	sinsp* m_inspector;
@@ -24,6 +22,8 @@ public:
 	boolop m_last_boolop;
 	bool m_have_rel_expr;
 	int32_t m_nest_level;
+
+	lua_State* m_ls;
 
 	friend class lua_parser_cbacks;
 };
