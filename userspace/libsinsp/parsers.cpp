814e548e22da642673cbe376c42ec6e889854615
Luca Marturana <luca@sysdig.com>
2016-02-24 14:18:13 -0800

    Avoid memory leak, fixes #537

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -630,7 +630,10 @@ void sinsp_parser::store_event(sinsp_evt *evt)
 	// Copy the data
 	//
 	auto tinfo = evt->m_tinfo;
-	tinfo->m_lastevent_data = reserve_event_buffer();
+	if(tinfo->m_lastevent_data == NULL)
+	{
+		tinfo->m_lastevent_data = reserve_event_buffer();
+	}
 	memcpy(tinfo->m_lastevent_data, evt->m_pevt, elen);
 	tinfo->m_lastevent_cpuid = evt->get_cpuid();
 
@@ -3341,7 +3344,10 @@ void sinsp_parser::parse_select_poll_epollwait_enter(sinsp_evt *evt)
 		return;
 	}
 
-	evt->m_tinfo->m_lastevent_data = reserve_event_buffer();
+	if(evt->m_tinfo->m_lastevent_data == NULL)
+	{
+		evt->m_tinfo->m_lastevent_data = reserve_event_buffer();
+	}
 	*(uint64_t*)evt->m_tinfo->m_lastevent_data = evt->get_ts();
 }
 void sinsp_parser::parse_fcntl_enter(sinsp_evt *evt)
