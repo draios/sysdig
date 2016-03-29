508dfb0037eea632bf16aaf5428d9131717a44e0
Luca Marturana <luca@sysdig.com>
2016-03-29 14:07:28 +0200

    Sendmsg fix on 32bit

--- driver/ppm_events.c
+++ driver/ppm_events.c
@@ -1283,7 +1283,10 @@ int32_t parse_readv_writev_bufs(struct event_filler_arguments *args, const struc
 			/*
 			 * Retrieve the FD. It will be used for dynamic snaplen calculation.
 			 */
-			syscall_get_arguments(current, args->regs, 0, 1, &val);
+			if (!args->is_socketcall)
+				syscall_get_arguments(current, args->regs, 0, 1, &val);
+			else
+				val = args->socketcall_args[0];
 			args->fd = (int)val;
 
 			/*
