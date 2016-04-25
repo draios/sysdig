de7e3eb86a7e2bfcae47d79513833f9d35e9398a
Loris Degioanni <loris@sysdig.com>
2016-04-25 15:30:57 -0700

    temporarily disable the tracer views

--- userspace/sysdig/chisels/v_spans_summary.lua
+++ userspace/sysdig/chisels/v_spans_summary.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-view_info = 
+disabled_view_info =
 {
 	id = "spans_summary",
 	name = "Spans Summary",
