c598a1ee57c9327fa9aadb45740f0d5ca5ef4bc3
ltagliamonte <luigi.tagliamonte86@gmail.com>
2016-06-20 17:20:56 -0700

    install_if_not_present

--- .travis-scripts/osx/install.sh
+++ .travis-scripts/osx/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-brew install cmake
-brew install luajit
+function install_if_not_present(){
+	brew ls | grep ${1}
+	if [[ ${?} -ne 0 ]]; then
+		brew install ${1}
+	else
+		echo "dependency ${1} already installed"
+	fi
+}
+install_if_not_present "cmake"
+install_if_not_present "luajit"
