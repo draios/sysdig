93a4e89a24073f5c6b957530a939f6d2972cdb32
Loris Degioanni <loris@sysdig.com>
2016-07-08 16:59:39 -0700

    sidemenu rendering bug

--- userspace/libsinsp/cursescomponents.cpp
+++ userspace/libsinsp/cursescomponents.cpp
@@ -196,9 +196,9 @@ curses_table_sidemenu::curses_table_sidemenu(sidemenu_type type, sinsp_cursesui*
 	m_selct_ori = m_selct;
 	m_type = type;
 
-	if(m_selct > (int32_t)(m_h - 1))
+	if(m_selct > (int32_t)(m_h - 2))
 	{
-		m_firstrow = m_selct;
+		m_firstrow = m_selct - (int32_t)(m_h - 2);
 	}
 }
 
