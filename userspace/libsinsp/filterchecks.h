9daaa9265104cfde1bd7e3d55d593a4dc295ab14
Mark Stemm <mark.stemm@gmail.com>
2016-07-11 14:44:09 -0700

    Implement "in (...)" as hash table lookups.
    Currently, a filtering expression with "x in (a, b c, ...)" is
    transformed into "x=a or x=b or x=c or ...". This can be slow for very
    long sets of a, b, c, especially when x has to be extracted from the
    event over and over.
    
    Replace this with a set membership test using unordered_set for
    PT_CHARBUF types.
    
    In filter.cpp, for CO_IN operators check the type of the operand. If
    it's PT_CHARBUF, loop over the items in the (a, b, c) set and call
    add_filter_value() for each. Otherwise, the current behavior is kept.
    
    sinsp_filter_check::add_filter_value now saves pointers to the filter
    values in the unordered_set m_val_storages_members, containing pairs
    of (pointer, length). Note that these are only pointers--the actual
    values are still held in m_val_storages.
    
    In sinsp_filter_check::flt_compare, for CO_IN operators simply check the
    unordered_set and if a match is found return true. This used to be dead
    code given how all CO_INs were replaced with a sequence of x=a or x=b or
    ..., but is used again.
    
    Custom functors g_hash_membuf/g_equal_to_membuf hash and compare the
    pointer-to-datas. When compiling with gcc, this simply uses gnu's
    built-in hash function for a pointer and length, which is quite
    fast. Otherwise, a standalone function is used.

--- userspace/libsinsp/filterchecks.h
+++ userspace/libsinsp/filterchecks.h
@@ -17,6 +17,7 @@ along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
+#include <unordered_set>
 #include <json/json.h>
 #include "k8s.h"
 #include "mesos.h"
@@ -40,6 +41,43 @@ public:
 	ppm_param_type m_type;
 	string m_name;
 	string m_description;
+};
+
+// Used for CO_IN filterchecks using PT_CHARBUFs to allow for quick
+// multi-value comparisons. Should also work for any filtercheck with
+// a buffer and length. When compiling with gnu compilers, use the
+// built in but not standard _hash_impl::hash function, which uses
+// murmurhash2 and is quite fast. Otherwise, uses
+// http://www.cse.yorku.ca/~oz/hash.html.
+
+// Used by m_val_storages_members
+typedef pair<uint8_t *, uint32_t> filter_value_member_t;
+
+struct g_hash_membuf
+{
+	size_t operator()(filter_value_member_t val) const
+	{
+#ifdef __GNUC__
+		return std::_Hash_impl::hash(val.first, val.second);
+#else
+		size_t hash = 5381;
+		for(uint8_t *p = val.first; p-val.first < val.second; p++)
+		{
+			int c = *p;
+
+			hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		}
+		return hash;
+#endif
+	}
+};
+
+struct g_equal_to_membuf
+{
+	bool operator()(filter_value_member_t a, filter_value_member_t b) const
+	{
+		return (strcmp((const char *) a.first, (const char *) b.first) == 0);
+	}
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -148,6 +186,10 @@ protected:
 	vector<vector<uint8_t>> m_val_storages;
 	inline uint8_t* filter_value_p(uint16_t i = 0) { return &m_val_storages[i][0]; }
 	inline vector<uint8_t> filter_value(uint16_t i = 0) { return m_val_storages[i]; }
+
+	unordered_set<filter_value_member_t,
+		g_hash_membuf,
+		g_equal_to_membuf> m_val_storages_members;
 
 	const filtercheck_field_info* m_field;
 	filter_check_info m_info;
