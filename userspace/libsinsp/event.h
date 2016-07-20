cc0fa90626688b77358126229fb9bb989989d20e
Mark Stemm <mark.stemm@gmail.com>
2016-07-19 17:51:57 -0700

    Move evttype filters into own class.
    Thinking ahead to an embeddable falco, move evttype filters into its own
    class sinsp_evttype_filter. The inspector creates one of these objects
    on demand whenever add_evttype_filter is called. Otherwise, from falco's
    view the interface hasn't changed.

--- userspace/libsinsp/event.h
+++ userspace/libsinsp/event.h
@@ -403,6 +403,7 @@ VISIBILITY_PRIVATE
 	friend class sinsp_analyzer;
 	friend class sinsp_filter_check_event;
 	friend class sinsp_filter_check_thread;
+	friend class sinsp_evttype_filter;
 	friend class sinsp_dumper;
 	friend class sinsp_analyzer_fd_listener;
 	friend class sinsp_analyzer_parsers;
