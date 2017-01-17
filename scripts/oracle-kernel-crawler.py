9f0b27d2faf8f2a175b7e76a78c98b837679e01d
Brett <brett@sysdig.com>
2017-01-17 15:27:44 -0800

    Don't build UEK2 since it's a 2.6.39 kernel

--- scripts/oracle-kernel-crawler.py
+++ scripts/oracle-kernel-crawler.py
@@ -15,21 +15,20 @@ repos = {
             # yum.oracle.com has a bad cert, so use http instead of https
             "root": "http://yum.oracle.com/",
             "discovery_pattern": "/html/body//h3/a[regex:test(@href, 'oracle-linux-6\.html')]/@href",
-            "sub_discovery_pattern": "/html/body//h3[regex:test(., '^UEK Release [0-9]+:')]/a[regex:test(@href, 'x86_64/index.html')]/@href",
+            "sub_discovery_pattern": "/html/body//h3[regex:test(., '^UEK Release [3-9]:')]/a[regex:test(@href, 'x86_64/index.html')]/@href",
             "page_pattern": "/html/body//a[regex:test(@href, '^getPackage/kernel-uek-(devel-)?[0-9].*\.rpm$')]/@href",
         }
     ],
 
     "OL7-UEK": [
         {
-            # yum.oracle.com has a bad cert, so use http instead of https
             "root": "http://yum.oracle.com/",
             "discovery_pattern": "/html/body//h3/a[regex:test(@href, 'oracle-linux-7\.html')]/@href",
-            "sub_discovery_pattern": "/html/body//h3[regex:test(., '^UEK Release [0-9]+:')]/a[regex:test(@href, 'x86_64/index.html')]/@href",
+            "sub_discovery_pattern": "/html/body//h3[regex:test(., '^UEK Release [3-9]:')]/a[regex:test(@href, 'x86_64/index.html')]/@href",
             "page_pattern": "/html/body//a[regex:test(@href, '^getPackage/kernel-uek-(devel-)?[0-9].*\.rpm$')]/@href",
         }
     ],
-    
+
     "Oracle-RHCK": [
         {
             "root": "http://yum.oracle.com/",
