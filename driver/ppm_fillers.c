acf954e3348af8c45b9d2413d337c18fc90431c6
arossert <arossert@paypal.com>
2017-04-29 13:19:50 +0300

    Implement probe_kernel_read() for older kernels

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -1013,6 +1013,24 @@ static int compat_accumulate_argv_or_env(compat_uptr_t argv,
 
 #endif
 
+// probe_kernel_read() only added in kernel 2.6.26
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 26)
+long probe_kernel_read(void *dst, const void *src, size_t size)
+{
+	long ret;
+	mm_segment_t old_fs = get_fs();
+
+	set_fs(KERNEL_DS);
+	pagefault_disable();
+	ret = __copy_from_user_inatomic(dst,
+									(__force const void __user *)src, size);
+	pagefault_enable();
+	set_fs(old_fs);
+
+	return ret ? -EFAULT : 0;
+}
+#endif
+
 static int ppm_get_tty(void)
 {
 	/* Locking of the signal structures seems too complicated across
@@ -1029,8 +1047,6 @@ static int ppm_get_tty(void)
 	int index;
 	int tty_nr = 0;
 
-	// probe_kernel_read() only added in kernel 2.6.26
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 26)
 	sig = current->signal;
 	if (!sig)
 		return 0;
@@ -1057,7 +1073,6 @@ static int ppm_get_tty(void)
 		return 0;
 
 	tty_nr = new_encode_dev(MKDEV(major, minor_start) + index);
-#endif
 
 	return tty_nr;
 }
