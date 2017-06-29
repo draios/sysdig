2fdb2a8c0db436caa46c221db37d5031b25b4649
Thejass Krishnan <tjskrishna@gmail.com>
2017-06-28 17:44:43 -0700

    Fixed kernel style, Removed extra new lines, Removed un-used variables sysdig-CLA-1.0-signed-off-by: Thejas Krishnan tjskrishna@gmail.com

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -529,59 +529,53 @@ static inline u32 open_modes_to_scap(unsigned long modes)
 {
 	u32 res = 0;
 
-
 	if (modes & S_IRUSR)
-		res |=PPM_S_IRUSR;
+		res |= PPM_S_IRUSR;
 
 	if (modes & S_IWUSR)
-		res |=PPM_S_IWUSR;
+		res |= PPM_S_IWUSR;
 
 	if (modes &  S_IXUSR)
-		res |=PPM_S_IXUSR;
-	
+		res |= PPM_S_IXUSR;
+
 	/*
 	* PPM_S_IRWXU == S_IRUSR | S_IWUSR | S_IXUSR 
 	*/
 
-
-
 	if (modes & S_IRGRP)
-		res |=PPM_S_IRGRP;
+		res |= PPM_S_IRGRP;
 
 	if (modes & S_IWGRP)
-		res |=PPM_S_IWGRP;
+		res |= PPM_S_IWGRP;
 
 	if (modes & S_IXGRP)
-		res |=PPM_S_IXGRP;
+		res |= PPM_S_IXGRP;
 
 	/*
 	* PPM_S_IRWXG == S_IRGRP | S_IWGRP | S_IXGRP 
 	*/
 
-
-
 	if (modes & S_IROTH)
-		res |=PPM_S_IROTH;
+		res |= PPM_S_IROTH;
 
 	if (modes & S_IWOTH)
-		res |=PPM_S_IWOTH;
+		res |= PPM_S_IWOTH;
 
 	if (modes & S_IXOTH)
-		res |=PPM_S_IXOTH;
+		res |= PPM_S_IXOTH;
 	
 	/*
 	* PPM_S_IRWXO == S_IROTH | S_IWOTH | S_IXOTH
 	*/
 	
-
 	if (modes & S_ISUID)
-		res |=PPM_S_ISUID;
+		res |= PPM_S_ISUID;
 
 	if (modes & S_ISGID)
-		res |=PPM_S_ISGID;
+		res |= PPM_S_ISGID;
 
 	if (modes & S_ISVTX)
-		res |=PPM_S_ISVTX;
+		res |= PPM_S_ISVTX;
 
 	return res;
 }
