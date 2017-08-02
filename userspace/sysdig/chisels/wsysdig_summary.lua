624cde9b38d43bedbdef70820b43f071762130ae
Loris Degioanni <loris@sysdig.com>
2017-08-02 10:20:33 +0200

    a bunch of work on the wsysdig summarizer chisel

--- userspace/sysdig/chisels/wsysdig_summary.lua
+++ userspace/sysdig/chisels/wsysdig_summary.lua
@@ -27,28 +27,41 @@ local json = require ("dkjson")
 local gsummary = {} -- The global summary
 local ssummary = {} -- Last sample's summary
 local nintervals = 0
+local file_cache_exists = false
 
--- Argument notification callback
 function on_set_arg(name, val)
 	return false
 end
 
+-------------------------------------------------------------------------------
+-- Summary handling helpers
+-------------------------------------------------------------------------------
+function create_category()
+	return {tot=0, max=0, timeLine={}}
+end
+
 function reset_summary(s)
-	s.nSpawnedProcs = 0
-	s.nFileOpensAll = 0
-	s.nFileOpensWrite = 0
-	s.nSysFileOpensAll = 0
-	s.nSysFileOpensWrite = 0
+	s.SpawnedProcs = create_category()
+	s.FileOpensAll = create_category()
+	s.FileOpensWrite = create_category()
+	s.SysFileOpensAll = create_category()
+	s.SysFileOpensWrite = create_category()
 end
 
-function add_summaries(dst, src)
-	dst.nSpawnedProcs = dst.nSpawnedProcs + src.nSpawnedProcs
-	dst.nFileOpensAll = dst.nFileOpensAll + src.nFileOpensAll
-	dst.nFileOpensWrite = dst.nFileOpensWrite + src.nFileOpensWrite
-	dst.nSysFileOpensAll = dst.nSysFileOpensAll + src.nSysFileOpensAll
-	dst.nSysFileOpensWrite = dst.nSysFileOpensWrite + src.nSysFileOpensWrite
+function add_summaries(ts_s, ts_ns, dst, src)
+	for k, v in pairs(src) do
+		dst[k].tot = dst[k].tot + v.tot
+		if v.tot > dst[k].max then
+			dst[k].max = v.tot 
+		end
+		local tl = dst[k].timeLine
+		tl[#tl+1] = {t = sysdig.make_ts(ts_s, ts_ns), v=v.tot}
+	end
 end
 
+-------------------------------------------------------------------------------
+-- Helpers to dig into event data
+-------------------------------------------------------------------------------
 function string.starts(big_str, small_str)
    return string.sub(big_str, 1, string.len(small_str)) == small_str
 end
@@ -70,8 +83,10 @@ function is_system_dir(filename)
 	return false
 end
 
--- Initialization callback
-function on_init()
+-------------------------------------------------------------------------------
+-- Initialization callbacks
+-------------------------------------------------------------------------------
+function on_init()	
     chisel.set_interval_ns(100000000)
 
     reset_summary(gsummary)
@@ -90,7 +105,22 @@ function on_init()
 	return true
 end
 
+function on_capture_start()
+--[[
+	local dirname = sysdig.get_evtsource_name() .. '_wd_index'
+	local f = io.open(dirname .. '/summary.json', "r")
+	if f ~= nil then
+		f:close()
+		file_cache_exists = true
+		sysdig.end_capture()
+	end
+]]--
+	return true
+end
+
+-------------------------------------------------------------------------------
 -- Event callback
+-------------------------------------------------------------------------------
 function on_event()
 	local etype = evt.field(fetype)
 	local dir = evt.field(fdir)
@@ -100,24 +130,24 @@ function on_event()
 	if dir ~= nil and dir == '<' then
 		if rawres ~= nil and rawres >= 0 then
 			if etype == 'execve' then
-				ssummary.nSpawnedProcs = ssummary.nSpawnedProcs + 1
+				ssummary.SpawnedProcs.tot = ssummary.SpawnedProcs.tot + 1
 			elseif etype == 'open' or etype == 'openat' then
 				local flags = evt.field(fflags)
 				if flags == nil then
 					return
 				end
 
-				ssummary.nFileOpensAll = ssummary.nFileOpensAll + 1
+				ssummary.FileOpensAll.tot = ssummary.FileOpensAll.tot + 1
 
 				if string.find(flags, 'O_RDWR') or string.find(flags, 'O_WRONLY') then
-					ssummary.nFileOpensWrite = ssummary.nFileOpensWrite + 1
+					ssummary.FileOpensWrite.tot = ssummary.FileOpensWrite.tot + 1
 				end
 
 				if is_system_dir(fdname) then
-					ssummary.nSysFileOpensAll = ssummary.nSysFileOpensAll + 1
+					ssummary.SysFileOpensAll.tot = ssummary.SysFileOpensAll.tot + 1
 
 					if string.find(flags, 'O_RDWR') or string.find(flags, 'O_WRONLY') then
-						ssummary.nSysFileOpensWrite = ssummary.nSysFileOpensWrite + 1
+						ssummary.SysFileOpensWrite.tot = ssummary.SysFileOpensWrite.tot + 1
 					end
 				end
 			end
@@ -127,42 +157,97 @@ function on_event()
 	return true
 end
 
+-------------------------------------------------------------------------------
 -- Periodic timeout callback
+-------------------------------------------------------------------------------
 function on_interval(ts_s, ts_ns, delta)	
-	add_summaries(gsummary, ssummary)
+	add_summaries(ts_s, ts_ns, gsummary, ssummary)
 	reset_summary(ssummary)
 
 	if nintervals % 20 == 0 then
 		print('{"progress": ' .. sysdig.get_read_progress() .. ' },')
+		io.flush(stdout)
 	end
 	nintervals = nintervals + 1
 
 	return true
 end
 
--- Called by the engine at the end of the capture
-function on_capture_end(ts_s, ts_ns, delta)
-	add_summaries(gsummary, ssummary)
-	local sstr = json.encode(gsummary, { indent = true })
---	print(sstr)
+-------------------------------------------------------------------------------
+-- End of capture output generation
+-------------------------------------------------------------------------------
+function build_output()
+	local res = {}
 
---print(sysdig.get_evtsource_name())
+	res[#res+1] = {
+		name = 'Spawned Processes',
+		desc = 'Number of new programs that have been executed during the observed interval',
+		data = gsummary.SpawnedProcs
+	}
+
+	res[#res+1] = {
+		name = 'Files Opened',
+		desc = 'XXX',
+		data = gsummary.FileOpensAll
+	}
+
+	res[#res+1] = {
+		name = 'Files Opened for Writing',
+		desc = 'XXX',
+		data = gsummary.FileOpensWrite
+	}
+
+	res[#res+1] = {
+		name = 'System Files Opened',
+		desc = 'XXX',
+		data = gsummary.SysFileOpensAll
+	}
+
+	res[#res+1] = {
+		name = 'System Files Opened for Writing',
+		desc = 'XXX',
+		data = gsummary.SysFileOpensWrite
+	}
+
+	resstr = json.encode(res, { indent = true })
+	return resstr
+end
+
+-- Callback called by the engine at the end of the capture
+function on_capture_end(ts_s, ts_ns, delta)
+	local sstr = ''
 	local dirname = sysdig.get_evtsource_name() .. '_wd_index'
 
-	os.execute('rm -fr ' .. dirname .. " 2> /dev/null")
-	os.execute('mkdir ' .. dirname .. " 2> /dev/null")
+--	if file_cache_exists then
+if false then
+		local f = io.open(dirname .. '/summary.json', "r")
+		if f == nil then
+			print('{"progress": 100, "error": "can\'t read the trace file index" }')
+			print(']}')
+			return false
+		end
 
-	local f = io.open(dirname .. '/summary.json', "w")
-	if f == nil then
-		print('{"progress": 100, "error": "can\'t create the trace file index" }')
-		print(']}')
-		return false
+		sstr = f:read("*all")
+		f:close()
+	else
+		add_summaries(ts_s, ts_ns, gsummary, ssummary)
+		sstr = build_output()
+
+		os.execute('rm -fr ' .. dirname .. " 2> /dev/null")
+		os.execute('mkdir ' .. dirname .. " 2> /dev/null")
+
+		local f = io.open(dirname .. '/summary.json', "w")
+		if f == nil then
+			print('{"progress": 100, "error": "can\'t create the trace file index" }')
+			print(']}')
+			return false
+		end
+
+		f:write(sstr)
+		f:close()
 	end
 
-	f:write(sstr)
-	f:close()
-
-	print('{"progress": 100}')
+	print('{"progress": 100, "data": '.. sstr ..'}')
 	print(']}')
 
 	return true
