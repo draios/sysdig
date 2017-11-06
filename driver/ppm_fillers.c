c759d00b61efe92d1d798c86e0744ffba528d774
arossert <arossert@paypal.com>
2017-11-06 16:14:36 +0200

    fixed kernel version check sysdig-CLA-1.0-contributing-entity: Amir Rossert sysdig-CLA-1.0-signed-off-by: John Tsai johntsai@paypal.com

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -1549,7 +1549,7 @@ cgroups_error:
 		/*
 		 * pgrp
 		 */
-#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 24)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 24)
 		res = val_to_ring(args, (int64_t)task_pgrp_nr(current), 0, false, 0);
 #else
 		res = val_to_ring(args, (int64_t)process_group(current), 0, false, 0);
