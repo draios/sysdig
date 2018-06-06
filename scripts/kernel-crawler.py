7ed325998db4e726654f05b39d579dd8a6faa5d2
anoop-sysd <31664950+anoop-sysd@users.noreply.github.com>
2018-06-06 16:42:17 -0700

    SMAGENT-834: Fix URL for Fedora updates (#1098)
    * SMAGENT-834: Fix URL for Fedora updates
    
    * SMAGENT-834: fix updates URL for f28.

--- scripts/kernel-crawler.py
+++ scripts/kernel-crawler.py
@@ -116,7 +116,16 @@ repos = {
             "root" : "https://mirrors.kernel.org/fedora/updates/",
             "discovery_pattern": "/html/body//a[regex:test(@href, '^2[2-9]/$')]/@href",
             "subdirs" : [
-                "x86_64/k/"
+                "x86_64/Packages/k/"
+            ],
+            "page_pattern" : "/html/body//a[regex:test(@href, '^kernel-(core|devel)-[0-9].*\.rpm$')]/@href"
+        },
+
+        {
+            "root" : "https://mirrors.kernel.org/fedora/updates/",
+            "discovery_pattern": "/html/body//a[regex:test(@href, '^2[2-9]/$')]/@href",
+            "subdirs" : [
+                "Everything/x86_64/Packages/k/"
             ],
             "page_pattern" : "/html/body//a[regex:test(@href, '^kernel-(core|devel)-[0-9].*\.rpm$')]/@href"
         },
