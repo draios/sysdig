ec0814073e022163a056e801a322fdefd948defa
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2018-08-02 18:34:56 +0200

    Support logging elapsed time on tracers
    Apart from having the tracers in the first place, allow setting
    a timeout, after which we log a message to bring our attention
    to the problem in the first place.

--- userspace/libsinsp/tracer_emitter.h
+++ userspace/libsinsp/tracer_emitter.h
@@ -6,8 +6,10 @@
 class tracer_emitter
 {
 public:
-	tracer_emitter(std::string tag);
-	tracer_emitter(std::string tag, const tracer_emitter &parent);
+	static const uint64_t no_timeout = ~0ULL;
+
+	tracer_emitter(std::string tag, uint64_t timeout_ns=no_timeout);
+	tracer_emitter(std::string tag, const tracer_emitter &parent, uint64_t timeout_ns=no_timeout);
 	~tracer_emitter();
 
 	tracer_emitter() = delete;
@@ -17,15 +19,18 @@ public:
 	// Stop is only needed if you want the exit
 	// event before the instance gets destructed,
 	// i.e. goes out of scope
-	void stop();
+	uint64_t stop();
 	static void set_enabled(bool enabled) { m_enabled = enabled; }
 
 private:
 	void start();
 	void write_tracer(const bool enter);
 	const std::string& tag() const { return m_tag; }
+	uint64_t elapsed_time() const;
 
 	const std::string m_tag;
+	const uint64_t m_start_ns = 0;
+	const uint64_t m_timeout_ns = 0;
 	bool m_exit_written = false;
 	static bool m_enabled;
 };
