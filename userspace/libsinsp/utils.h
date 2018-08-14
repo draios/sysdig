3ab4e81ccd711a25b15269dcd2de80fe6811140b
Mattia Pagnozzi <mattia.pagnozzi@gmail.com>
2018-08-14 22:46:46 +0200

    Add run_on_interval template class It can be used to run code on a specific interval.

--- userspace/libsinsp/utils.h
+++ userspace/libsinsp/utils.h
@@ -381,3 +381,39 @@ inline void hash_combine(std::size_t &seed, const T& val)
 {
 	seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 }
+
+/**
+ * Often we need to run something on an interval
+ * usually we need to store last_run_ts compare to now
+ * and run it
+ * This micro-class makes this easier
+ */
+class run_on_interval
+{
+public:
+	inline run_on_interval(uint64_t interval);
+
+	template<typename Callable>
+	inline void run(const Callable& c, uint64_t now = sinsp_utils::get_current_time_ns());
+	uint64_t interval() const { return m_interval; }
+	void interval(uint64_t i) { m_interval = i; }
+private:
+	uint64_t m_last_run_ns;
+	uint64_t m_interval;
+};
+
+run_on_interval::run_on_interval(uint64_t interval):
+		m_last_run_ns(0),
+		m_interval(interval)
+{
+}
+
+template<typename Callable>
+void run_on_interval::run(const Callable& c, uint64_t now)
+{
+	if(now - m_last_run_ns > m_interval)
+	{
+		c();
+		m_last_run_ns = now;
+	}
+}
