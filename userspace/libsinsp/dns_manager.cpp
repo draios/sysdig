35cef60d739b5deb9ba7ce65bad3ea4eaf577530
Mattia Pagnozzi <mattia.pagnozzi@gmail.com>
2018-08-29 16:39:41 +0200

    Add a new class to manage domain name resolutions sinsp_dns_manager is in charge of resolving a list of domain names and, using a separate thread, keeping them in sync over time.

---
+++ userspace/libsinsp/dns_manager.cpp
@@ -0,0 +1,195 @@
+/*
+Copyright (C) 2013-2018 Draios inc.
+
+This file is part of sysdig.
+
+sysdig is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+sysdig is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "dns_manager.h"
+
+void sinsp_dns_resolver::refresh(uint64_t erase_timeout, uint64_t base_refresh_timeout, uint64_t max_refresh_timeout, std::future<void> f_exit)
+{
+#ifdef HAS_CAPTURE
+	sinsp_dns_manager &manager = sinsp_dns_manager::get();
+	while(true)
+	{
+		if(!manager.m_cache.empty())
+		{
+			std::list<std::string> to_delete;
+
+			uint64_t ts = sinsp_utils::get_current_time_ns();
+
+			for(auto &it: manager.m_cache)
+			{
+				const std::string &name = it.first;
+				sinsp_dns_manager::dns_info &info = it.second;
+
+				if((ts > info.m_last_used_ts) &&
+				   (ts - info.m_last_used_ts) > erase_timeout)
+				{
+					// remove the entry if it's hasn't been used for a whole hour
+					to_delete.push_back(name);
+				}
+				else if(ts > (info.m_last_resolve_ts + info.m_timeout))
+				{
+					sinsp_dns_manager::dns_info refreshed_info = manager.resolve(name, ts);
+					info.m_timeout = base_refresh_timeout;
+					info.m_last_resolve_ts = ts;
+
+					if(refreshed_info != info)
+					{
+						info = refreshed_info;
+					}
+					else if(info.m_timeout < max_refresh_timeout)
+					{
+						// double the timeout until 320 secs
+						info.m_timeout <<= 1;
+					}
+				}
+			}
+			if(!to_delete.empty())
+			{
+				manager.m_erase_mutex.lock();
+				for(const auto &name : to_delete)
+				{
+					manager.m_cache.unsafe_erase(name);
+				}
+				manager.m_erase_mutex.unlock();
+			}
+		}
+
+		if(f_exit.wait_for(std::chrono::nanoseconds(base_refresh_timeout)) == std::future_status::ready)
+		{
+			break;
+		}
+	}
+#endif
+}
+
+#ifdef HAS_CAPTURE
+inline sinsp_dns_manager::dns_info sinsp_dns_manager::resolve(const std::string &name, uint64_t ts)
+{
+	dns_info dinfo;
+
+	struct addrinfo hints, *result, *rp;
+	memset(&hints, 0, sizeof(struct addrinfo));
+
+	// Allow IPv4 or IPv6, all socket types, all protocols
+	hints.ai_family = AF_UNSPEC;
+
+	int s = getaddrinfo(name.c_str(), NULL, &hints, &result);
+	if (!s && result)
+	{
+		for (rp = result; rp != NULL; rp = rp->ai_next)
+		{
+			if(rp->ai_family == AF_INET)
+			{
+				dinfo.m_v4_addrs.insert(((struct sockaddr_in*)rp->ai_addr)->sin_addr.s_addr);
+			}
+			else // AF_INET6
+			{
+				std::array<uint32_t, 4> v6_ip;
+				memcpy(v6_ip.data(), ((struct sockaddr_in6*)rp->ai_addr)->sin6_addr.s6_addr, 16);
+				dinfo.m_v6_addrs.insert(v6_ip);
+			}
+		}
+		freeaddrinfo(result);
+	}
+	return dinfo;
+}
+#endif
+
+bool sinsp_dns_manager::match(const char *name, uint32_t *addr, uint64_t ts, bool is_v6)
+{
+#ifdef HAS_CAPTURE
+	if(!m_resolver)
+	{
+		m_resolver = new thread(sinsp_dns_resolver::refresh, m_erase_timeout, m_base_refresh_timeout, m_max_refresh_timeout, m_exit_signal.get_future());
+	}
+
+	string sname = string(name);
+
+	m_erase_mutex.lock();
+
+	if(m_cache.find(sname) == m_cache.end())
+	{
+		dns_info dinfo = resolve(sname, ts);
+		dinfo.m_timeout = m_base_refresh_timeout;
+		dinfo.m_last_resolve_ts = ts;
+		m_cache[sname] = dinfo;
+	}
+
+	m_cache[sname].m_last_used_ts = ts;
+	dns_info &dinfo = m_cache[sname];
+
+	m_erase_mutex.unlock();
+
+	if(is_v6)
+	{
+		std::array<uint32_t, 4> v6_ip;
+		memcpy(v6_ip.data(), addr, 16);
+		return dinfo.m_v6_addrs.find(v6_ip) != dinfo.m_v6_addrs.end();
+	}
+	else
+	{
+		return dinfo.m_v4_addrs.find(*addr) != dinfo.m_v4_addrs.end();
+	}
+#endif
+	return false;
+}
+
+string sinsp_dns_manager::name_of(uint32_t *addr, bool is_v6)
+{
+	string ret;
+
+#ifdef HAS_CAPTURE
+
+	if(!m_cache.empty())
+	{
+		m_erase_mutex.lock();
+		for(auto &it: m_cache)
+		{
+			const std::string &name = it.first;
+			sinsp_dns_manager::dns_info &info = it.second;
+
+			if(is_v6)
+			{
+				std::array<uint32_t, 4> v6_ip;
+				memcpy(v6_ip.data(), addr, 16);
+				if (info.m_v6_addrs.find(v6_ip) != info.m_v6_addrs.end())
+				{
+					ret = name;
+					break;
+				}
+			}
+			else if(info.m_v4_addrs.find(*addr) != info.m_v4_addrs.end())
+			{
+				ret = name;
+				break;
+			}
+		}
+		m_erase_mutex.unlock();
+	}
+#endif
+	return ret;
+}
+
+void sinsp_dns_manager::cleanup()
+{
+	if(m_resolver)
+	{
+		m_exit_signal.set_value();
+		m_resolver->join();
+	}
+}
---
+++ userspace/libsinsp/dns_manager.h
@@ -0,0 +1,125 @@
+/*
+Copyright (C) 2013-2018 Draios inc.
+
+This file is part of sysdig.
+
+sysdig is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+sysdig is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <future>
+#include <mutex>
+#ifdef HAS_CAPTURE
+#include "tbb/concurrent_unordered_map.h"
+#endif
+#include "sinsp.h"
+
+
+struct sinsp_dns_resolver
+{
+	static void refresh(uint64_t erase_timeout, uint64_t base_refresh_timeout, uint64_t max_refresh_timeout, std::future<void> f_exit);
+};
+
+class sinsp_dns_manager
+{
+public:
+
+	bool match(const char *name, uint32_t *addr, uint64_t ts, bool is_v6);
+	string name_of(uint32_t *addr, bool is_v6);
+
+	void cleanup();
+
+        static sinsp_dns_manager& get()
+        {
+            static sinsp_dns_manager instance;
+            return instance;
+        };
+
+	void set_erase_timeout(uint64_t ns)
+	{
+		m_erase_timeout = ns;
+	};
+	void set_base_refresh_timeout(uint64_t ns)
+	{
+		m_base_refresh_timeout = ns;
+	};
+	void set_max_refresh_timeout(uint64_t ns)
+	{
+		m_max_refresh_timeout = ns;
+	};
+
+	size_t size()
+	{
+#ifdef HAS_CAPTURE
+		return m_cache.size();
+#else
+		return 0;
+#endif
+	};
+
+private:
+
+	sinsp_dns_manager() :
+		m_erase_timeout(3600 * ONE_SECOND_IN_NS),
+		m_base_refresh_timeout(10 * ONE_SECOND_IN_NS),
+		m_max_refresh_timeout(320 * ONE_SECOND_IN_NS)
+	{};
+        sinsp_dns_manager(sinsp_dns_manager const&) = delete;
+        void operator=(sinsp_dns_manager const&) = delete;
+
+#ifdef HAS_CAPTURE
+	struct dns_info
+	{
+		bool operator==(const dns_info &other) const
+		{
+			return m_v4_addrs == other.m_v4_addrs && m_v6_addrs == other.m_v6_addrs;
+		};
+		bool operator!=(const dns_info &other) const
+		{
+			return !operator==(other);
+		};
+
+		uint64_t m_timeout;
+		uint64_t m_last_resolve_ts;
+		uint64_t m_last_used_ts;
+		std::set<uint32_t> m_v4_addrs;
+		std::set<std::array<uint32_t, 4>> m_v6_addrs;
+	};
+
+	static inline dns_info resolve(const std::string &name, uint64_t ts);
+
+	typedef tbb::concurrent_unordered_map<std::string, dns_info> c_dns_table;
+	c_dns_table m_cache;
+#endif
+
+	// tbb concurrent unordered map is not thread-safe for deletions,
+	// so we still need a mutex, but the chances of waiting are really
+	// low, since we will almost never do an erase.
+	std::mutex m_erase_mutex;
+
+	// used to let m_resolver when to terminate
+	std::promise<void> m_exit_signal;
+
+	std::thread *m_resolver;
+
+	uint64_t m_erase_timeout;
+	uint64_t m_base_refresh_timeout;
+	uint64_t m_max_refresh_timeout;
+
+	friend sinsp_dns_resolver;
+};
