afcec5344921ded6ab8b4e2b28d7872ea39a4c38
Mark Stemm <mark.stemm@gmail.com>
2018-09-26 12:09:47 -0700

    Change Brendan Gregg chisels to apache 2 license
    We obtained his permission to change the license to apache 2.0.

--- userspace/sysdig/chisels/fileslower.lua
+++ userspace/sysdig/chisels/fileslower.lua
@@ -1,4 +1,23 @@
 --[[
+Copyright (C) 2014 Brendan Gregg.
+
+This file is part of sysdig.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+--]]
+
+--[[
 fileslower.lua - trace file I/O slower than a given threshold.
 
 USAGE: sysdig -c fileslower min_ms
@@ -15,19 +34,6 @@ chisel to change this behavior.
 Note: The file I/O traced is those matched by the sysdig filter:
 "evt.is_io=true and fd.type=file".
 
-Copyright (C) 2014 Brendan Gregg.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 -- Chisel description
