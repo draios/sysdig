e404c50c346ce1a2750446aeaac59f4062f33b0a
Mark Stemm <mark.stemm@sysdig.com>
2018-09-28 13:17:07 -0700

    Change license apache2 (#1233)
    * Change license in COPYING, c/c++ code
    
    Replace the commented license notice in c/c++ userspace code with the
    apache 2 license, and change the top level COPYING license to apache 2.
    
    Still need to change other code e.g. lua, shell scripts, as well as
    changing the license for the driver.
    
    * Change lua code from gpl to apache license
    
    Change lua code that had a clear gpl comment block at the beginning of
    the file to apache 2. This doesn't yet cover all files, but the
    exceptions will be handled next.
    
    * Update scripts to use apache 2.0 license.
    
    * Update chisels COPYING to Apache 2 license.
    
    * Change copyright for Luca-written files
    
    He was a sysdig employee at the time.
    
    * Add apache copyright to files that didn't have one
    
    For files that had no copyright notice, add the apache copyright notice.
    
    * Use correct copyright years.
    
    * Change Brendan Gregg chisels to apache 2 license
    
    We obtained his permission to change the license to apache 2.0.
    
    * Add dba sysdig to copyright notices
    
    * Change driver license to dual MIT + GPL
    
    In the driver directory, include both the MIT and GPLv2 licenses. In
    each file, add a notice that the code is dual licensed under both MIT
    and GPLv2.
    
    Update the README to note that the driver is dual-licensed while
    the userspace programs are apache 2 licensed.
    
    * Update remaining chisels to use apache license
    
    We got permission from the author via email.
    
    * Change new .c files to apache license
    
    Were recently added and picked up during rebase.

--- driver/ppm_cputime.c
+++ driver/ppm_cputime.c
@@ -1,3 +1,11 @@
+/*
+
+Copyright (c) 2013-2018 Draios Inc. dba Sysdig.
+
+This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+or GPL2.txt for full copies of the license.
+
+*/
 #include <linux/version.h>
 
 // These function are taken from the linux kernel and are used only
--- userspace/sysdig/chisels/memcachelog.lua
+++ userspace/sysdig/chisels/memcachelog.lua
@@ -1,4 +1,24 @@
 --[[
+
+Copyright (C) 2015 Donatas Abraitis.
+
+This file is part of sysdig.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+--]]
+
+--[[
 memcachelog.lua - show most used memcached keys.
 
 USAGE: sysdig -c memcachelog <get|set>
@@ -10,20 +30,6 @@ USAGE: sysdig -c memcachelog <get|set>
   sysdig -c memcachelog 'set 1000'  # show memcached set utilization which object's size is higher than 1000
 
 By default it will print both methods.
-
-Copyright (C) 2015 Donatas Abraitis.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 -- Chisel description
