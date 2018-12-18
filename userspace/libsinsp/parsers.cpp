c7bea7bead037b95517f3bdcfaac68117da1867e
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2018-12-18 15:53:23 +0100

    Move Docker support to a separate file. Still needs more work.

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -50,6 +50,8 @@ limitations under the License.
 bool should_drop(sinsp_evt *evt);
 #endif
 
+#include "container_docker_common.h"
+
 extern sinsp_protodecoder_list g_decoderlist;
 extern sinsp_evttables g_infotables;
 
