33c3686c0087236f372f94861a6433f039d1b7a4
Andy Dalton <andy.dalton@sysdig.com>
2019-01-31 13:47:52 -0500

    Remove commented out headers

--- userspace/libsinsp/sinsp_public.h
+++ userspace/libsinsp/sinsp_public.h
@@ -20,9 +20,7 @@ limitations under the License.
 
 #ifdef _WIN32
 #define SINSP_PUBLIC __declspec(dllexport)
-//#include <Ws2tcpip.h>
 #else
 #define SINSP_PUBLIC
-//#include <arpa/inet.h>
 #endif
 
