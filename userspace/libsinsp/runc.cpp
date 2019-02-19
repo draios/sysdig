a46968c532ec9e14e586f1914afe0cc95e1c3a64
Krishnan Ramkumar <krishnan.ramkumar@sysdig.com>
2019-02-19 12:56:35 -0800

    Fix edge cases and short-circuit for strings which are smaller in length

--- userspace/libsinsp/runc.cpp
+++ userspace/libsinsp/runc.cpp
@@ -37,6 +37,11 @@ static_assert(REPORTED_CONTAINER_ID_LENGTH <= CONTAINER_ID_LENGTH, "Reported con
 // Otherwise return false and leave container_id unchanged
 bool match_one_container_id(const std::string &cgroup, const std::string &prefix, const std::string &suffix, std::string &container_id)
 {
+	// Short and return early for trivial candidates
+	if(cgroup.length() < (CONTAINER_ID_LENGTH + prefix.length() + suffix.length())) {
+		return false;
+	}
+	
 	// We search the cgroup string from backwards to front; splitting
 	// it using the '/' char delimiter. We use this as the basis for
 	// analysing cgroups. Once we have a string of desired length , we then
@@ -49,7 +54,7 @@ bool match_one_container_id(const std::string &cgroup, const std::string &prefix
 		std::string sub_string = cgroup.substr(start_pos, end_pos - start_pos + 1);
 
 		// Only proceed for those substrings that are of required length
-		if(sub_string.length() >= CONTAINER_ID_LENGTH) {
+		if(sub_string.length() >= ( CONTAINER_ID_LENGTH + prefix.length() + suffix.length())) {
 			// prefix and suffix matching
 			start_c_id = sub_string.find(prefix);
 			size_t end_c_id = sub_string.rfind(suffix);
@@ -72,10 +77,18 @@ bool match_one_container_id(const std::string &cgroup, const std::string &prefix
 
 		// if the program reaches here; the current
 		// sub_string that we processed does not match
-		// a container id. Continue!
+		// a container id. Reset limits for next iteration
+		// and continue
+
+		if(start_pos == 0) {
+			// Already at the start ; no use continuing
+			return false;
+		}
+
+		// Reset limits for next iteration
 		end_pos = start_pos - 1;
 		start_pos--;
-		if(end_pos <= CONTAINER_ID_LENGTH) {
+		if(end_pos < (CONTAINER_ID_LENGTH + prefix.length() + suffix.length())) {
 			// Remaining string is too short to find container id
 			// No use continuing.
 			return false;
