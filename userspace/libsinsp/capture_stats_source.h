cc4c2e32aa2df4cd6c02955482e19696613f16a1
Andy Dalton <andy.dalton@sysdig.com>
2019-03-26 15:19:04 -0400

    Incremental update

--- userspace/libsinsp/capture_stats_source.h
+++ userspace/libsinsp/capture_stats_source.h
@@ -13,11 +13,25 @@ class scap_stats;
 
 /**
  * Interface to an object that can provide capture statistics.
+ *
+ * Note that this intention here is to apply the Interface Segregation
+ * Principle (ISP) to class sinsp.  Some clients of sinsp need only the
+ * get_capture_stats() API, and this interface exposes only that API.  Do
+ * not add additional APIs here.  If some client of sinsp needs a different
+ * set of APIs, introduce a new interface.
  */
 class SINSP_PUBLIC capture_stats_source
 {
 public:
 	virtual ~capture_stats_source() = default;
 
+	/**
+	 * Fill the given structure with statistics about the currently
+	 * open capture.
+	 *
+	 * @note This may not work for a file-based capture source.
+	 *
+	 * @param[out] stats The capture statistics
+	 */
 	virtual void get_capture_stats(scap_stats* stats) = 0;
 };
