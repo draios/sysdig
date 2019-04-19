ac8b33098cd60f41a15376e950dbc2a2e2f5d9e2
Mark Stemm <mark.stemm@gmail.com>
2019-04-18 17:18:31 -0700

    Use right header file on osx.

--- userspace/libsinsp/grpc_channel_registry.h
+++ userspace/libsinsp/grpc_channel_registry.h
@@ -20,7 +20,11 @@ limitations under the License.
 #pragma once
 
 #include <memory>
-#include <grpc++/grpc++.h>
+#ifdef GRPC_INCLUDE_IS_GRPCPP
+#	include <grpcpp/grpcpp.h>
+#else
+#	include <grpc++/grpc++.h>
+#endif
 
 namespace libsinsp
 {
