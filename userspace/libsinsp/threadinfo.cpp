29fc05b39dd8dcb02aaa44312ad1227ab569a008
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2019-04-22 19:26:36 +0200

    Allow lookup of specific cgroups of a thread by subsystem name

--- userspace/libsinsp/threadinfo.cpp
+++ userspace/libsinsp/threadinfo.cpp
@@ -893,6 +893,21 @@ uint64_t sinsp_threadinfo::get_fd_limit()
 	return get_main_thread()->m_fdlimit;
 }
 
+const std::string& sinsp_threadinfo::get_cgroup(const std::string& subsys) const
+{
+	static const std::string notfound = "/";
+
+	for(const auto& it : m_cgroups)
+	{
+		if(it.first == subsys)
+		{
+			return it.second;
+		}
+	}
+
+	return notfound;
+}
+
 void sinsp_threadinfo::traverse_parent_state(visitor_func_t &visitor)
 {
 	// Use two pointers starting at this, traversing the parent
