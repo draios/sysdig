b0c3ed90181282169971365de7cc868138b2d432
Yathi Naik <yathi@sysdig.com>
2019-06-05 13:09:48 -0700

    fix indentation

--- userspace/libsinsp/container_engine/docker_common.cpp
+++ userspace/libsinsp/container_engine/docker_common.cpp
@@ -320,11 +320,11 @@ bool docker_async_source::parse_docker(std::string &container_id, sinsp_containe
 			Json::Value img_root;
 			if(reader.parse(img_json, img_root))
 			{
-			    // img_root["RepoDigests"] contains only digests for images pulled from registries.
-			    // If an image gets retagged and is never pushed to any registry, we will not find
-			    // that entry in container->m_imagerepo. Also, for locally built images we have the
-			    // same issue. This leads to container->m_imagedigest being empty as well.
-			    unordered_set<std::string> imageDigestSet;
+				// img_root["RepoDigests"] contains only digests for images pulled from registries.
+				// If an image gets retagged and is never pushed to any registry, we will not find
+				// that entry in container->m_imagerepo. Also, for locally built images we have the
+				// same issue. This leads to container->m_imagedigest being empty as well.
+				unordered_set<std::string> imageDigestSet;
 				for(const auto& rdig : img_root["RepoDigests"])
 				{
 					if(rdig.isString())
@@ -363,7 +363,7 @@ bool docker_async_source::parse_docker(std::string &container_id, sinsp_containe
 				// Case 1: One repo digest with many tags.
 				// Case 2: Many repo digests with the same digest value.
 				if(container->m_imagedigest.empty() && imageDigestSet.size() == 1) {
-				    container->m_imagedigest = *imageDigestSet.begin();
+					container->m_imagedigest = *imageDigestSet.begin();
 				}
 			}
 			else
