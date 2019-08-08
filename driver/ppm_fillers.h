c5431155e4f0f5f9e85479532825018f63b90082
Lorenzo Fontana <lo@linux.com>
2019-08-08 23:54:05 +0000

    new(driver): filler for chmod
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- driver/ppm_fillers.h
+++ driver/ppm_fillers.h
@@ -109,6 +109,7 @@ or GPL2.txt for full copies of the license.
 	FN(sys_bpf_x)				\
 	FN(sys_unlinkat_x)			\
 	FN(sys_fchmodat_x)			\
+	FN(sys_chmod_x)				\
 	FN(sys_mkdirat_x)			\
 	FN(sys_openat_x)			\
 	FN(sys_linkat_x)			\
