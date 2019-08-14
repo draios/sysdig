4b8f52caf8daa9231daa884c6286aa367025fe36
bryanseay <bryan@sysdig.com>
2019-08-14 21:21:09 +0000

    Revert "Support lookup of per-subsystem cgroup mount points"
    This reverts commit 5308360b839aacee50bc0c584fc5753a3a301034.

--- userspace/libsinsp/sinsp.cpp
+++ userspace/libsinsp/sinsp.cpp
@@ -42,7 +42,6 @@ limitations under the License.
 #include "k8s_api_handler.h"
 #ifdef HAS_CAPTURE
 #include <curl/curl.h>
-#include <mntent.h>
 #endif
 #endif
 
@@ -2538,56 +2537,3 @@ bool sinsp_thread_manager::remove_inactive_threads()
 
 	return res;
 }
-
-#ifdef HAS_CAPTURE
-std::shared_ptr<std::string> sinsp::lookup_cgroup_dir(const string& subsys)
-{
-	shared_ptr<string> cgroup_dir;
-	static std::unordered_map<std::string, std::shared_ptr<std::string>> cgroup_dir_cache;
-
-	const auto& it = cgroup_dir_cache.find(subsys);
-	if(it != cgroup_dir_cache.end())
-	{
-		return it->second;
-	}
-
-	// Look for mount point of cgroup filesystem
-	// It should be already mounted on the host or by
-	// our docker-entrypoint.sh script
-	if(strcmp(scap_get_host_root(), "") != 0)
-	{
-		// We are inside our container, so we should use the directory
-		// mounted by it
-		auto cgroup = std::string(scap_get_host_root()) + "/cgroup/" + subsys;
-		cgroup_dir = std::make_shared<std::string>(cgroup);
-	}
-	else
-	{
-		struct mntent mntent_buf = {};
-		char mntent_string_buf[4096];
-		FILE* fp = setmntent("/proc/mounts", "r");
-		struct mntent* entry = getmntent_r(fp, &mntent_buf,
-			mntent_string_buf, sizeof(mntent_string_buf));
-		while(entry != nullptr)
-		{
-			if(strcmp(entry->mnt_type, "cgroup") == 0 &&
-			   hasmntopt(entry, subsys.c_str()) != NULL)
-			{
-				cgroup_dir = std::make_shared<std::string>(entry->mnt_dir);
-				break;
-			}
-			entry = getmntent(fp);
-		}
-		endmntent(fp);
-	}
-	if(!cgroup_dir)
-	{
-		return std::make_shared<std::string>();
-	}
-	else
-	{
-		cgroup_dir_cache[subsys] = cgroup_dir;
-		return cgroup_dir;
-	}
-}
-#endif
