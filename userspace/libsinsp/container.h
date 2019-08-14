7dde279e73313ff106ad5d85a6ffa6b481ebc62a
bryanseay <bryan@sysdig.com>
2019-08-14 21:24:11 +0000

    Revert "Make container.h self-sufficient"
    This reverts commit f65dfeb76c0bf3f895f21640bbbfdde46f05693e.

--- userspace/libsinsp/container.h
+++ userspace/libsinsp/container.h
@@ -20,10 +20,6 @@ limitations under the License.
 #pragma once
 
 #include <functional>
-#include <list>
-#include <memory>
-#include <string>
-#include <unordered_map>
 
 #include "container_info.h"
 
@@ -35,25 +31,22 @@ limitations under the License.
 
 #include "container_engine/container_engine.h"
 
-struct scap_dumper;
-class sinsp_evt;
-
 class sinsp_container_manager
 {
 public:
 	sinsp_container_manager(sinsp* inspector);
 	virtual ~sinsp_container_manager();
 
-	const std::unordered_map<std::string, sinsp_container_info>* get_containers();
+	const unordered_map<string, sinsp_container_info>* get_containers();
 	bool remove_inactive_containers();
 	void add_container(const sinsp_container_info& container_info, sinsp_threadinfo *thread);
-	sinsp_container_info * get_container(const std::string &id);
+	sinsp_container_info * get_container(const string &id);
 	void notify_new_container(const sinsp_container_info& container_info);
 	template<typename E> bool resolve_container_impl(sinsp_threadinfo* tinfo, bool query_os_for_missing_info);
 	template<typename E1, typename E2, typename... Args> bool resolve_container_impl(sinsp_threadinfo* tinfo, bool query_os_for_missing_info);
 	bool resolve_container(sinsp_threadinfo* tinfo, bool query_os_for_missing_info);
-	void dump_containers(struct scap_dumper* dumper);
-	std::string get_container_name(sinsp_threadinfo* tinfo);
+	void dump_containers(scap_dumper_t* dumper);
+	string get_container_name(sinsp_threadinfo* tinfo);
 
 	// Set tinfo's m_category based on the container context.  It
 	// will *not* change any category to NONE, so a threadinfo
@@ -61,7 +54,7 @@ public:
 	// across execs e.g. "sh -c /bin/true" execing /bin/true.
 	void identify_category(sinsp_threadinfo *tinfo);
 
-	bool container_exists(const std::string& container_id) const {
+	bool container_exists(const string& container_id) const {
 		return m_containers.find(container_id) != m_containers.end();
 	}
 
@@ -79,17 +72,17 @@ public:
 	void set_cri_timeout(int64_t timeout_ms);
 	sinsp* get_inspector() { return m_inspector; }
 private:
-	std::string container_to_json(const sinsp_container_info& container_info);
-	bool container_to_sinsp_event(const std::string& json, sinsp_evt* evt, std::shared_ptr<sinsp_threadinfo> tinfo);
-	std::string get_docker_env(const Json::Value &env_vars, const std::string &mti);
+	string container_to_json(const sinsp_container_info& container_info);
+	bool container_to_sinsp_event(const string& json, sinsp_evt* evt, shared_ptr<sinsp_threadinfo> tinfo);
+	string get_docker_env(const Json::Value &env_vars, const string &mti);
 
 	std::list<std::unique_ptr<libsinsp::container_engine::resolver>> m_container_engines;
 
 	sinsp* m_inspector;
-	std::unordered_map<std::string, sinsp_container_info> m_containers;
+	unordered_map<string, sinsp_container_info> m_containers;
 	uint64_t m_last_flush_time_ns;
-	std::list<new_container_cb> m_new_callbacks;
-	std::list<remove_container_cb> m_remove_callbacks;
+	list<new_container_cb> m_new_callbacks;
+	list<remove_container_cb> m_remove_callbacks;
 
 	friend class test_helper;
 };
