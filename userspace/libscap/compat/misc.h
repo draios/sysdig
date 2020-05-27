f77842171bff11de13c51d4c2babe365e70f0998
Omer Azaria <omer.azaria@sysdig.com>
2020-05-27 12:33:07 -0700

    Move static inline implementation to scap_bpf

--- userspace/libscap/compat/misc.h
+++ userspace/libscap/compat/misc.h
@@ -41,16 +41,4 @@ limitations under the License.
 #define O_TMPFILE 020200000
 #endif
 
-static int sys_bpf(enum bpf_cmd cmd, union bpf_attr *attr, unsigned int size)
-{
-	return syscall(__NR_bpf, cmd, attr, size);
-}
-
-static int sys_perf_event_open(struct perf_event_attr *attr,
-			       pid_t pid, int cpu, int group_fd,
-			       unsigned long flags)
-{
-	return syscall(__NR_perf_event_open, attr, pid, cpu, group_fd, flags);
-}
-
 #endif
