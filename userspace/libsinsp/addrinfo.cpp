8bbebd960ff893577e27be5919fb33cf275d4f4d
Leonardo Di Donato <leodidonato@gmail.com>
2020-09-07 16:18:29 +0000

    new(userspace/libsinsp): callback fro async dns resolution
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ userspace/libsinsp/addrinfo.cpp
@@ -0,0 +1,27 @@
+#include "addrinfo.h"
+
+#include <ares.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+void ares_cb(void *arg, int status, int timeouts, struct hostent *host)
+{
+    if (status == ARES_EDESTRUCTION)
+    {
+        return;
+    }
+
+    if (status == ARES_SUCCESS)
+    {
+        struct in_addr addr;
+        char *p;
+        p = host->h_addr_list[0];
+        memcpy(&addr, p, sizeof(struct in_addr));
+        ares_cb_result *res = reinterpret_cast<ares_cb_result *>(arg);
+        auto addr_str = std::string(inet_ntoa(addr));
+        res->address = addr_str;
+        res->addr = addr;
+        res->done = true;
+    }
+}
