310fbb253ff6c41939c82305e553d5dbf0eb126f
Leonardo Di Donato <leodidonato@gmail.com>
2020-09-08 14:20:01 +0000

    fix(userspace/libsinsp): addrinfo helper needed only when build is not a minimal one
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/libsinsp/addrinfo.h
+++ userspace/libsinsp/addrinfo.h
@@ -1,4 +1,23 @@
 
+/*
+Copyright (C) 2020 Sysdig Inc.
+
+This file is part of sysdig.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef MINIMAL_BUILD
 #pragma once
 
 #include <netinet/in.h>
@@ -13,3 +32,5 @@ struct ares_cb_result
 };
 
 void ares_cb(void *arg, int status, int timeouts, struct hostent *host);
+
+#endif // MINIMAL_BUILD
