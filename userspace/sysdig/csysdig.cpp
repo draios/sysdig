60147ef746e84c8677599d7123cc5f7fac8b8188
Loris Degioanni <loris@sysdig.com>
2020-10-05 13:50:30 -0700

    fix compilation under windows

--- userspace/sysdig/csysdig.cpp
+++ userspace/sysdig/csysdig.cpp
@@ -31,7 +31,9 @@ limitations under the License.
 #include <sinsp.h>
 #include "chisel.h"
 #ifdef HAS_CAPTURE
+#ifndef WIN32
 #include "driver_config.h"
+#endif // WIN32
 #endif // HAS_CAPTURE
 #include "sysdig.h"
 #include "fields_info.h"
