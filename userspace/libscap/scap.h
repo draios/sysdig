9e55e1aaee2962c6fb6cac036176b81fbe7d49e8
Leonardo Di Donato <leodidonato@gmail.com>
2020-11-11 15:11:28 +0000

    new(userspace/libscap): interface for scap
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/libscap/scap.h
+++ userspace/libscap/scap.h
@@ -656,6 +656,21 @@ uint64_t scap_max_buf_used(scap_t* handle);
    SCAP_EOF when the end of an offline capture is reached.
    On Failure, SCAP_FAILURE is returned and scap_getlasterr() can be used to obtain the cause of the error.
 */
+int32_t scap_next_per_cpu(scap_t* handle, OUT scap_evt** pevent, uint16_t pcpuid);
+
+/*!
+  \brief Get the next event from the from the given capture instance
+
+  \param handle Handle to the capture instance.
+  \param pevent User-provided event pointer that will be initialized with address of the event.
+  \param pcpuid User-provided event pointer that will be initialized with the ID if the CPU
+    where the event was captured.
+
+  \return SCAP_SUCCESS if the call is successful and pevent and pcpuid contain valid data.
+   SCAP_TIMEOUT in case the read timeout expired and no event is available.
+   SCAP_EOF when the end of an offline capture is reached.
+   On Failure, SCAP_FAILURE is returned and scap_getlasterr() can be used to obtain the cause of the error.
+*/
 int32_t scap_next(scap_t* handle, OUT scap_evt** pevent, OUT uint16_t* pcpuid);
 
 /*!
