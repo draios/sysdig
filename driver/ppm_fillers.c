d23837414cb36f9b5a3f755794039ff995d20ab0
lbubuntu <lucamontechiesi@gmail.com>
2020-12-15 12:19:52 -0800

    update(driver): copy_file_range filler definitions
    sysdig-CLA-1.0-signed-off-by: Luca Montechiesi <lucamontechiesi@gmail.com>

--- driver/ppm_fillers.c
+++ driver/ppm_fillers.c
@@ -4295,6 +4295,170 @@ int f_sys_sendfile_x(struct event_filler_arguments *args)
 	return add_sentinel(args);
 }
 
+int f_sys_copy_file_range_e(struct event_filler_arguments *args)
+{
+	unsigned long val;
+	int res;
+	loff_t off_in;
+	loff_t off_out;
+
+	/*
+	 * fd_in
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 0, 1, &val);
+	res = val_to_ring(args, val, 0, false, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * off_in
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 1, 1, &val);
+
+	if (val != 0) {
+#ifdef CONFIG_COMPAT
+		if (!args->compat) {
+#endif
+			res = ppm_copy_from_user(&off_in, (void *)val, sizeof(loff_t));
+#ifdef CONFIG_COMPAT
+		} else {
+			res = ppm_copy_from_user(&off_in, (void *)compat_ptr(val), sizeof(compat_loff_t));
+		}
+#endif
+		if (unlikely(res))
+			val = 0;
+		else
+			val = off_in;
+	}
+
+	res = val_to_ring(args, val, 0, true, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+
+	/*
+	 * fd_out
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 2, 1, &val);
+	res = val_to_ring(args, val, 0, false, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * off_out
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 3, 1, &val);
+
+	if (val != 0) {
+#ifdef CONFIG_COMPAT
+		if (!args->compat) {
+#endif
+			res = ppm_copy_from_user(&off_out, (void *)val, sizeof(loff_t));
+#ifdef CONFIG_COMPAT
+		} else {
+			res = ppm_copy_from_user(&off_out, (void *)compat_ptr(val), sizeof(compat_loff_t));
+		}
+#endif
+		if (unlikely(res))
+			val = 0;
+		else
+			val = off_out;
+	}
+
+	res = val_to_ring(args, val, 0, true, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * len
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 4, 1, &val);
+	res = val_to_ring(args, val, 0, false, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * flags
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 5, 1, &val);
+	res = val_to_ring(args, val, 0, false, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+
+	return add_sentinel(args);
+}
+
+int f_sys_copy_file_range_x(struct event_filler_arguments *args)
+{
+	unsigned long val;
+	int res;
+	int64_t retval;
+	loff_t off_in;
+	loff_t off_out;
+
+	/*
+	 * res
+	 */
+	retval = (int64_t)syscall_get_return_value(current, args->regs);
+	res = val_to_ring(args, retval, 0, false, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * off_in
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 1, 1, &val);
+
+	if (val != 0) {
+#ifdef CONFIG_COMPAT
+		if (!args->compat) {
+#endif
+			res = ppm_copy_from_user(&off_in, (void *)val, sizeof(loff_t));
+#ifdef CONFIG_COMPAT
+		} else {
+			res = ppm_copy_from_user(&off_in, (void *)compat_ptr(val), sizeof(compat_loff_t));
+		}
+#endif
+		if (unlikely(res))
+			val = 0;
+		else
+			val = off_in;
+	}
+
+	res = val_to_ring(args, val, 0, true, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+	/*
+	 * off_out
+	 */
+	syscall_get_arguments_deprecated(current, args->regs, 3, 1, &val);
+
+	if (val != 0) {
+#ifdef CONFIG_COMPAT
+		if (!args->compat) {
+#endif
+			res = ppm_copy_from_user(&off_out, (void *)val, sizeof(loff_t));
+#ifdef CONFIG_COMPAT
+		} else {
+			res = ppm_copy_from_user(&off_out, (void *)compat_ptr(val), sizeof(compat_loff_t));
+		}
+#endif
+		if (unlikely(res))
+			val = 0;
+		else
+			val = off_out;
+	}
+
+	res = val_to_ring(args, val, 0, true, 0);
+	if (unlikely(res != PPM_SUCCESS))
+		return res;
+
+
+	return add_sentinel(args);
+}
+
 int f_sys_quotactl_e(struct event_filler_arguments *args)
 {
 	unsigned long val;
