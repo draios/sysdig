81e9eac13e0e5244ca2e6016d47b77a0c8ac9468
Lorenzo Fontana <lo@linux.com>
2021-02-14 22:16:28 +0100

    chore: access fillers perf buffer
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- driver/bpf/test/test_fillers.c
+++ driver/bpf/test/test_fillers.c
@@ -1,10 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
 #include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/resource.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
 
 
 void set_rlimit_infinity(void)
@@ -14,12 +18,19 @@ void set_rlimit_infinity(void)
 	setrlimit(RLIMIT_MEMLOCK, &rinf);
 }
 
+static void bpf_handle_cb(void *ctx, int cpu, void *data, __u32 size)
+{
+	fprintf(stdout, "filler call available\n");
+}
+
+
 int main(int argc, char **argv)
 {
 	struct bpf_program *prog;
 	struct bpf_map *map;
 	struct bpf_object *obj;
 	struct bpf_object_load_attr load_attr = {};
+
 
 	char probe_path[256];
 
@@ -56,6 +67,8 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
+	struct bpf_map *perf_map = bpf_object__find_map_by_name(obj, "perf_map");
+
 	bpf_object__for_each_program(prog, obj)
 	{
 		const char *event_name = bpf_program__name(prog);
@@ -66,18 +79,26 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		// todo: externalize this in a test case
-		if(strcmp(event_name, "bpf_sys_renameat2_x") == 0)
+		// todo load the right tracepoint for every program, and make sure to deal with the non entry/exit ones
+		link = bpf_program__attach_raw_tracepoint(prog, "sys_exit");
+		if((unsigned long)link < 0)
 		{
-			link = bpf_program__attach_raw_tracepoint(prog, "sys_exit");
-			if((unsigned long)link < 0) {
-				fprintf(stderr,"ERROR: could not attach to the sys_exit raw tracepoint for prog: %s", event_name);
-				bpf_link__destroy(link);
-				goto cleanup;
-			}
-			// todo(fntlnz): fetch the data from the map and do the assertions
+			fprintf(stderr, "ERROR: could not attach to the sys_exit raw tracepoint for prog: %s", event_name);
+			bpf_link__destroy(link);
+			goto cleanup;
 		}
 	}
+
+	struct perf_buffer_opts pb_opts = {};
+	pb_opts.sample_cb = bpf_handle_cb;
+	struct perf_buffer *pb;
+	pb = perf_buffer__new(bpf_map__fd(perf_map), 8, &pb_opts); // not sure about how many pages to access, check current code
+
+	while ((perf_buffer__poll(pb, -1)) >= 0) {
+		// todo(fntlnz): deal with threads and affinity here, also do timeouts
+	}
+
+	return EXIT_SUCCESS;
 cleanup:
 	bpf_object__close(obj);
 	return EXIT_FAILURE;
