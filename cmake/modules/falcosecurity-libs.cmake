7d71fdaffc74508ae0899f637576170606222f30
Mark Stemm <mark.stemm@gmail.com>
2021-12-08 12:45:37 -0800

    Update libs to version with better list fields support
    This has an improved list_fields() function with better output.
    
    sysdig-CLA-1.0-signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -29,8 +29,8 @@ else()
   # default below In case you want to test against another falcosecurity/libs version just pass the variable - ie., `cmake
   # -DFALCOSECURITY_LIBS_VERSION=dev ..`
   if(NOT FALCOSECURITY_LIBS_VERSION)
-    set(FALCOSECURITY_LIBS_VERSION "2160111cd088aea9ae2235d3385ecb0b1ab6623c")
-    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=f84edc4f7490064a0e2264594013c01c205bc5fc968376bfb0ecc17582e5e112")
+    set(FALCOSECURITY_LIBS_VERSION "7e832f61a05df68969697fd603594d65c4e14811")
+    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=1a81218425ec105e76a53a8baef0e734abf1b1739567c226e45c3d673f69014e")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
