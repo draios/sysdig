55063767213ccbe42425014029d613a23797651e
PoojaSoni96 <93122936+PoojaSoni96@users.noreply.github.com>
2021-12-09 17:45:39 +0530

    Update CMakeListsGtestInclude.cmake

--- CMakeListsGtestInclude.cmake
+++ CMakeListsGtestInclude.cmake
@@ -23,7 +23,7 @@ project(googletest-download NONE)
 include(ExternalProject)
 ExternalProject_Add(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
-  GIT_TAG           master
+  GIT_TAG           main
   SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
   BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
   CONFIGURE_COMMAND ""
