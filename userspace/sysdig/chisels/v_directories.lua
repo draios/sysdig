15fc84c93aa569d4ce17d47e94b0df7ec450d71a
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-12-14 20:44:42 +0100

    fix(chisels): added parentheses around ambiguos boolean filters
    sysdig-CLA-1.0-signed-off-by: Massimiliano Torromeo <massimiliano.torromeo@gmail.com>

--- userspace/sysdig/chisels/v_directories.lua
+++ userspace/sysdig/chisels/v_directories.lua
@@ -26,7 +26,7 @@ view_info =
 	tags = {"Default", "wsysdig"},
 	view_type = "table",
 	applies_to = {"", "container.id", "proc.pid", "thread.nametid", "proc.name", "thread.tid", "fd.sport", "fd.sproto", "evt.res", "k8s.pod.id", "k8s.rc.id", "k8s.rs.id", "k8s.svc.id", "k8s.ns.id", "marathon.app.id", "marathon.group.name", "mesos.task.id", "mesos.framework.name"},
-	filter = "fd.type=file or fd.type=directory and fd.name!=''",
+	filter = "(fd.type=file or fd.type=directory) and fd.name!=''",
 	use_defaults = true,
 	drilldown_target = "files",
 	columns = 
