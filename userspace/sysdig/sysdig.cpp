e1ae0d4a7aacd89896b127fc85ae45738ba52439
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-02-10 17:05:33 +0000

    gvisor
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- userspace/sysdig/sysdig.cpp
+++ userspace/sysdig/sysdig.cpp
@@ -1025,6 +1025,7 @@ sysdig_init_res sysdig_init(int argc, char **argv)
 	string cri_socket_path;
 #endif
 	bool udig = false;
+	bool gvisor = false;
 
 	// These variables are for the cycle_writer engine
 	int duration_seconds = 0;
@@ -1052,6 +1053,7 @@ sysdig_init_res sysdig_init(int argc, char **argv)
 		{"fatfile", no_argument, 0, 'F'},
 		{"filter-proclist", no_argument, 0, 0 },
 		{"seconds", required_argument, 0, 'G' },
+		{"gvisor", no_argument, 0, 'g'},
 		{"help", no_argument, 0, 'h' },
 		{"plugin", required_argument, 0, 'H' },
 		{"input", required_argument, 0, 'I' },
@@ -1126,7 +1128,7 @@ sysdig_init_res sysdig_init(int argc, char **argv)
                                         "AbB::c:"
                                         "C:"
                                         "dDEe:F"
-                                        "G:"
+                                        "G:g"
                                         "hH:I:i:jk:K:lLm:M:n:Pp:qRr:Ss:t:TU:uv"
                                         "W:"
                                         "w:xXz", long_options, &long_index)) != -1)
@@ -1213,6 +1215,9 @@ sysdig_init_res sysdig_init(int argc, char **argv)
 				inspector->set_fatfile_dump_mode(true);
 				break;
 			// Number of seconds between roll-over
+			case 'g':
+				gvisor = true;
+				break;
 			case 'G':
 				duration_seconds = atoi(optarg);
 				if(duration_seconds <= 0)
@@ -1832,9 +1837,12 @@ sysdig_init_res sysdig_init(int argc, char **argv)
 				{
 					inspector->open_udig();
 				}
+				else if (gvisor)
+				{
+					inspector->open_gvisor();
+				}
 				else
 				{
-
 					try
 					{
 						inspector->open("");
