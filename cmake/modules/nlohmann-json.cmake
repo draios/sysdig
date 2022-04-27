114436c1a45142ef73acfc2607fbc6572782160f
eiffel-fl <laniel_francis@privacyrequired.com>
2022-04-27 17:24:11 +0200

    cmake: Check USE_BUNDLED_DEPS before getting nlohmann-json. (#1869)
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- cmake/modules/nlohmann-json.cmake
+++ cmake/modules/nlohmann-json.cmake
@@ -16,13 +16,22 @@
 # limitations under the License.
 #
 
-set(NJSON_SRC "${PROJECT_BINARY_DIR}/njson-prefix/src/njson")
-message(STATUS "Using bundled nlohmann-json in '${NJSON_SRC}'")
-set(NJSON_INCLUDE_DIR "${NJSON_SRC}/single_include")
-ExternalProject_Add(
-  njson
-  URL "https://github.com/nlohmann/json/archive/v3.3.0.tar.gz"
-  URL_HASH "SHA256=2fd1d207b4669a7843296c41d3b6ac5b23d00dec48dba507ba051d14564aa801"
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND "")
+if(NOT USE_BUNDLED_DEPS)
+  find_path(NJSON_INCLUDE_DIR NAMES nlohmann/json.hpp)
+  if(NJSON_INCLUDE_DIR)
+    message(STATUS "Found njson: include: ${NJSON_INCLUDE_DIR}")
+  else()
+    message(FATAL_ERROR "Couldn't find system njson")
+  endif()
+else()
+  set(NJSON_SRC "${PROJECT_BINARY_DIR}/njson-prefix/src/njson")
+  message(STATUS "Using bundled nlohmann-json in '${NJSON_SRC}'")
+  set(NJSON_INCLUDE_DIR "${NJSON_SRC}/single_include")
+  ExternalProject_Add(
+    njson
+    URL "https://github.com/nlohmann/json/archive/v3.3.0.tar.gz"
+    URL_HASH "SHA256=2fd1d207b4669a7843296c41d3b6ac5b23d00dec48dba507ba051d14564aa801"
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND "")
+endif()
