13c85be42491b656104dfcd074335f1cd6753c94
Jason Dellaluce <jasondellaluce@gmail.com>
2022-07-15 08:53:15 +0000

    update: bump libs version to 3d1549ce1934f08b8e7959a0d38ce2b0f32bdcfe
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -32,8 +32,8 @@ else()
   # default below In case you want to test against another falcosecurity/libs version just pass the variable - ie., `cmake
   # -DFALCOSECURITY_LIBS_VERSION=dev ..`
   if(NOT FALCOSECURITY_LIBS_VERSION)
-    set(FALCOSECURITY_LIBS_VERSION "0.7.0")
-    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=3adc1620c0e830554a54cdd486158dc2c0c40552e113785b70fbbc99edb7d96f")
+    set(FALCOSECURITY_LIBS_VERSION "3d1549ce1934f08b8e7959a0d38ce2b0f32bdcfe")
+    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=ef86d62a99f14405a5770fab73905601b8eb23ea3ac43fcebf904b2cbec90706")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
