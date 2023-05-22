3d22309a7317d500f75d053af6f9e5b5b5728b40
Jason Dellaluce <jasondellaluce@gmail.com>
2023-05-22 09:56:04 +0000

    update(cmake): bump driver to 5.0.0+driver
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- cmake/modules/driver.cmake
+++ cmake/modules/driver.cmake
@@ -31,8 +31,8 @@ else()
   # In case you want to test against another driver version (or branch, or commit) just pass the variable -
   # ie., `cmake -DDRIVER_VERSION=dev ..`
   if(NOT DRIVER_VERSION)
-    set(DRIVER_VERSION "4.0.1+driver")
-    set(DRIVER_CHECKSUM "SHA256=424d647aa6b6fa45b6701c4bbc9293c161ed00a08254d83eea419c0a736f280d")
+    set(DRIVER_VERSION "5.0.0+driver")
+    set(DRIVER_CHECKSUM "SHA256=c988ca7ac7d174f62d1bfbaaca49efd117f7b329f474d1b46b643635b2e35083")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
